7819cfdb2e35c0829e4d3d98740c3c3002b2f4c6
AR123456 <coloradoz99@gmail.com>
2023-12-03 04:16:13 -0700

    variables

---
+++ Section 2 Variables and Data Types/03-variables/app.js
@@ -0,0 +1,22 @@
+// Ways to declare a variable
+// var, let, const
+
+// Naming Conventions
+// - Only letters, numbers, underscores and dollar signs
+// - Can't start with a number
+
+// Multi-Word Formatting
+// firstName     camelCase
+// first_name    underscore
+// FirstName     PascalCase - componet names
+// firstname     lowercase
+// With let, we can declare a value without assigning a value
+let score;
+score = 1;
+console.log(score);
+
+if (true) {
+  score = score + 1;
+}
+
+// console.log(score);
