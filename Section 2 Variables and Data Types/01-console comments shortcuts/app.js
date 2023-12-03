734e6d1e1ec5af6c6371ed269f41e7998f2422f8
AR123456 <coloradoz99@gmail.com>
2023-12-03 04:15:29 -0700

    console log and data types

---
+++ Section 2 Variables and Data Types/01-console comments shortcuts/app.js
@@ -0,0 +1,5 @@
+console.log(100);
+console.log("hello");
+console.table({ name: "Anne", email: "test@gmail.com" });
+const styles = "padding:100px;background-color:white;color:red";
+console.log("%cHello world", styles);
