8ae963a50fa5fb82f37a964a6192d7b629703235
AR123456 <coloradoz99@gmail.com>
2023-12-20 06:40:50 -0700

    remove() method

---
+++ 06-document-object-model/13-remove-elements/shopping-list/script.js
@@ -0,0 +1,6 @@
+// remove the clear button using  remove() Method
+const removeClearButton = () => {
+  clearBtn = document.querySelector("#clear");
+  clearBtn.remove();
+};
+// removeClearButton();
