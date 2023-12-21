a24aa31f53bb46fe786025c761406a68b95878c1
AR123456 <coloradoz99@gmail.com>
2023-12-21 06:01:49 -0700

    manipulating css with js set up

---
+++ 06-document-object-model/14-styles-classes/script.js
@@ -0,0 +1,8 @@
+const text = document.querySelector("p");
+const itemList = document.querySelector(".item-list");
+const items = document.querySelectorAll("li");
+
+const run = () => {};
+
+// set up the on click to run the run function
+document.querySelector("button").onClick = run;
