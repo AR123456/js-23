b7d18cf517f8bebba5cd5f0baf3326e478c43653
AR123456 <coloradoz99@gmail.com>
2023-12-23 06:41:51 -0700

    logging the class list

--- 06-document-object-model/14-styles-classes/script.js
+++ 06-document-object-model/14-styles-classes/script.js
@@ -6,7 +6,11 @@ const run = () => {
   // className - can get
   console.log(itemList.className);
   // or set
-  text.className = "dark";
+  // text.className = "dark";
+  // classList object- the better way to handle
+  // its a dom token list and similar to an array
+  console.log(itemList.classList);
+  //
 };
 
 // set up the on click to run the run function
