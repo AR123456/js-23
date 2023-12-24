21b57deda3322131935b738b4c720c317a00d1f0
AR123456 <coloradoz99@gmail.com>
2023-12-24 08:31:09 -0700

    let JS mimic the click and call the onClick function

--- 07-events/01-event-listeners/shopping-list/script.js
+++ 07-events/01-event-listeners/shopping-list/script.js
@@ -4,18 +4,25 @@ const clearBtn = document.querySelector("#clear");
 function onClear() {
   alert("cleared from the function");
 }
-clearBtn.onclick = onClear;
+// clearBtn.onclick = onClear;
 
-/// another way to write
-clearBtn.onclick = function () {
-  alert("cleared from the other way in line non es6");
-};
-/// ES 6 way to write
+// /// another way to write
+// clearBtn.onclick = function () {
+//   alert("cleared from the other way in line non es6");
+// };
+// /// ES 6 way to write
 
-clearBtn.onclick = () => alert("cleared from in line on click");
+// clearBtn.onclick = () => alert("cleared from in line on click");
 // add event listener allow one to pass in the type of event and hen the function
 // or can add the event in line
 // Use named function
 clearBtn.addEventListener("click", onClear);
-// passing in the function in line
-clearBtn.addEventListener("click", () => alert("I am in line and clearing"));
+// // passing in the function in line
+// clearBtn.addEventListener("click", () => alert("I am in line and clearing"));
+// // removeEventListener()
+// // here using in a setTimeout so will happen in 5 sec
+// setTimeout(() => {
+//   clearBtn.removeEventListener("click", onClear), 5000;
+// });
+// in this line JS is firing the event without a click
+setTimeout(() => clearBtn.click(), 5000);
