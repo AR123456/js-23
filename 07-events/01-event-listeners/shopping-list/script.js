2696f12bc87383b9614be49ad6402cd280a55c0f
AR123456 <coloradoz99@gmail.com>
2023-12-24 08:07:44 -0700

    in line on click

--- 07-events/01-event-listeners/shopping-list/script.js
+++ 07-events/01-event-listeners/shopping-list/script.js
@@ -2,16 +2,20 @@ const clearBtn = document.querySelector("#clear");
 
 // the function to run on the event
 function onClear() {
-  alert("clear");
+  alert("cleared from the function");
 }
-// clearBtn.onclick = onClear;
+clearBtn.onclick = onClear;
 
-// /// another way to write
-// clearBtn.onclick = function () {
-//   alert("cleared");
-// };
-// /// ES 6 way to write
-// clearBtn.onclick = () => alert("clear");
+/// another way to write
+clearBtn.onclick = function () {
+  alert("cleared from the other way in line non es6");
+};
+/// ES 6 way to write
+
+clearBtn.onclick = () => alert("cleared from in line on click");
 // add event listener allow one to pass in the type of event and hen the function
 // or can add the event in line
+// Use named function
 clearBtn.addEventListener("click", onClear);
+// passing in the function in line
+clearBtn.addEventListener("click", () => alert("I am in line and clearing"));
