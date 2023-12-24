a17112df011626ff94866d68b68ea129e1000602
AR123456 <coloradoz99@gmail.com>
2023-12-24 08:00:44 -0700

    using the preferred addeventlistener

--- 07-events/01-event-listeners/shopping-list/script.js
+++ 07-events/01-event-listeners/shopping-list/script.js
@@ -1,13 +1,17 @@
 const clearBtn = document.querySelector("#clear");
 
 // the function to run on the event
-// function onClear() {
-//   alert("clear");
-// }
+function onClear() {
+  alert("clear");
+}
 // clearBtn.onclick = onClear;
-/// another way to write
+
+// /// another way to write
 // clearBtn.onclick = function () {
 //   alert("cleared");
 // };
-/// ES 6 way to write
-clearBtn.onclick = () => alert("clear");
+// /// ES 6 way to write
+// clearBtn.onclick = () => alert("clear");
+// add event listener allow one to pass in the type of event and hen the function
+// or can add the event in line
+clearBtn.addEventListener("click", onClear);
