dfdcac324d09506c7c80f386829b8cbd72144497
AR123456 <coloradoz99@gmail.com>
2023-12-25 07:09:08 -0700

    add event listeners

--- 07-events/02-mouse-events/shopping-list/script.js
+++ 07-events/02-mouse-events/shopping-list/script.js
@@ -1,1 +1,15 @@
+// variables
+const logo = document.querySelector("img");
 
+// functions
+const onClick = () => {
+  console.log(logo);
+};
+const onDoubleClick = () => {
+  document.body.style.backgroundColor = "purple";
+};
+//////event listeners
+// click
+logo.addEventListener("click", onClick);
+// double click
+logo.addEventListener("dblclick", onDoubleClick);
