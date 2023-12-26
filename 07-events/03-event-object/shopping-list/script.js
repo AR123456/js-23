af80826410fe330c9aca39038c3659ab88b1ffad
AR123456 <coloradoz99@gmail.com>
2023-12-26 06:54:57 -0700

    looking at the event object

--- 07-events/03-event-object/shopping-list/script.js
+++ 07-events/03-event-object/shopping-list/script.js
@@ -1,14 +1,24 @@
-/*
-- `target` - The element that triggered the event
-- `currentTarget` - The element that the event listener is attached to (These are the same in this case
-- `type` - The type of event that was triggered
-- `timeStamp` - The time that the event was triggered
-- `clientX` - The x position of the mouse click relative to the window
-- `clientY` - The y position of the mouse click relative to the window
-- `offsetX` - The x position of the mouse click relative to the element
-- `offsetY` - The y position of the mouse click relative to the element
-- `pageX` - The x position of the mouse click relative to the page
-- `pageY` - The y position of the mouse click relative to the page
-- `screenX` - The x position of the mouse click relative to the screen
-- `screenY` - The y position of the mouse click relative to the screen
-*/
+const logo = document.querySelector("img");
+// adding event listener
+logo.addEventListener("click", function (e) {
+  console.log(e);
+});
+
+// logging out some stuff in the pointer event
+
+function onClick(e) {
+  // Event properties
+  console.log(e.target);
+  console.log(e.currentTarget);
+  console.log(e.type);
+  console.log(e.timeStamp);
+  console.log(e.clientX);
+  console.log(e.clientY);
+  console.log(e.offsetX);
+  console.log(e.offsetY);
+  console.log(e.pageX);
+  console.log(e.pageY);
+  console.log(e.screenX);
+  console.log(e.screenY);
+}
+logo.addEventListener("click", onClick);
