b8e8844f16ab9880f4ca37687a5406ebed5eb203
AR123456 <coloradoz99@gmail.com>
2023-12-28 05:45:27 -0700

    onkeyup on keydown

--- 07-events/04-keyboard-events/shopping-list/script.js
+++ 07-events/04-keyboard-events/shopping-list/script.js
@@ -1,7 +1,13 @@
 const itemInput = document.getElementById("item-input");
 
-const onKeyUp = () => console.log("keyup");
-const onKeyDown = () => console.log("keydown");
+const onKeyUp = (e) => {
+  console.log("keyup");
+};
+const onKeyDown = (e) => {
+  // you can get the key that is pressed
+  console.log(e.key);
+  console.log(e.keyCode);
+};
 
 itemInput.addEventListener("keyup", onKeyUp);
 itemInput.addEventListener("keydown", onKeyDown);
