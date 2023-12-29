60de76dbe8ee17075e180ade413182ff410f3fe9
AR123456 <coloradoz99@gmail.com>
2023-12-29 07:43:24 -0700

    loging some key events

--- 07-events/04-keyboard-events/shopping-list/script.js
+++ 07-events/04-keyboard-events/shopping-list/script.js
@@ -7,6 +7,15 @@ const onKeyDown = (e) => {
   // you can get the key that is pressed
   console.log(e.key);
   console.log(e.keyCode);
+  // keyCode
+  // https://www.toptal.com/developers/keycode/table-of-all-keycodes
+  if (e.keyCode === 13) {
+    alert("You pressed enter");
+  }
+  // code
+  if (e.code === "Digit1") {
+    console.log("You pressed 1");
+  }
 };
 
 itemInput.addEventListener("keyup", onKeyUp);
