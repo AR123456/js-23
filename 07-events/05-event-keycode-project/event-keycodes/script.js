8a21c2b141c4ea830c5f92db393786224c148962
AR123456 <coloradoz99@gmail.com>
2023-12-30 08:48:19 -0700

    js to get the events

--- 07-events/05-event-keycode-project/event-keycodes/script.js
+++ 07-events/05-event-keycode-project/event-keycodes/script.js
@@ -3,3 +3,11 @@
 //- ref 50 projects in 50 days course for one solution
 // create the key elements in the DOM - for in
 // html and css supplied
+const insert = document.querySelector("#insert");
+
+const onKeyDown = (e) => {
+  console.log(e.key);
+  console.log(e.keyCode);
+  console.log(e.code);
+};
+document.addEventListener("keydown", onKeyDown);
