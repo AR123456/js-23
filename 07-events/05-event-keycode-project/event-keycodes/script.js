01886d40f54258455b6226eac51be44fc33b4455
AR123456 <coloradoz99@gmail.com>
2023-12-30 08:47:56 -0700

    event keycode challenge starter

---
+++ 07-events/05-event-keycode-project/event-keycodes/script.js
@@ -0,0 +1,5 @@
+//  make this website  https://www.toptal.com/developers/keycode
+// show the e.key e.keyCode and e.code  on key down
+//- ref 50 projects in 50 days course for one solution
+// create the key elements in the DOM - for in
+// html and css supplied
