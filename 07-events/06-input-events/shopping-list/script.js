c8040cc8ec84a0f717efc785a0c8ca23e863a7ee
AR123456 <coloradoz99@gmail.com>
2023-12-31 07:37:06 -0700

    getting some elements

---
+++ 07-events/06-input-events/shopping-list/script.js
@@ -0,0 +1,4 @@
+itemInput = document.querySelector("#item-input");
+priorityInput = document.querySelector("#priority-input");
+checkbox = document.querySelector("#checkbox");
+heading = document.querySelector("h1");
