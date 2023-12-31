24f244de47e763aa8c3b11a0fd042c780132a76c
AR123456 <coloradoz99@gmail.com>
2023-12-31 07:42:02 -0700

    on input event

--- 07-events/06-input-events/shopping-list/script.js
+++ 07-events/06-input-events/shopping-list/script.js
@@ -1,4 +1,14 @@
+//text input
 itemInput = document.querySelector("#item-input");
+// select list
 priorityInput = document.querySelector("#priority-input");
+// check box input
 checkbox = document.querySelector("#checkbox");
 heading = document.querySelector("h1");
+
+// looking at the text input
+function onInput(e) {
+  console.log("input");
+}
+// the input event works with all the input types
+itemInput.addEventListener("input", onInput);
