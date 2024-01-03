fc24cacef24e15edd4adb404cbe2b58463189136
AR123456 <coloradoz99@gmail.com>
2024-01-03 06:26:28 -0700

    bubbling up to parents

--- 07-events/08-event-bubbling/shopping-list/script.js
+++ 07-events/08-event-bubbling/shopping-list/script.js
@@ -6,5 +6,17 @@ const form = document.querySelector("form");
 button.addEventListener("click", (e) => {
   //   e.preventDefault();
   alert("clicked");
-  e.stopPropagation();
+  //   e.stopPropagation();
+});
+// this will bubble up to its parents
+div.addEventListener("click", () => {
+  alert("Div was clicked");
+});
+// will bubble up to its parents
+form.addEventListener("click", () => {
+  alert("Form was clicked");
+});
+// will bubble up to its parents
+document.body.addEventListener("click", () => {
+  alert("Body was clicked");
 });
