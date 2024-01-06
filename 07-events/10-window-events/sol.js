b534d38c66308cf69104f07390ce8f5d39bb093b
AR123456 <coloradoz99@gmail.com>
2024-01-06 15:13:16 -0700

    js file loading, use of defer if in head

--- 07-events/10-window-events/sol.js
+++ 07-events/10-window-events/sol.js
@@ -1,44 +1,44 @@
-// On Page Load
+// On Page Load- old school if JS in head with out defer
 // window.onload = function () {
 //   document.querySelector('h1').textContent = 'Hello World';
 // };
 
-window.addEventListener('load', () => console.log('Page Loaded'));
+window.addEventListener("load", () => console.log("Page Loaded"));
 
 // On DOM Load
-window.addEventListener('DOMContentLoaded', () => console.log('DOM Loaded'));
+window.addEventListener("DOMContentLoaded", () => console.log("DOM Loaded"));
 
-console.log('Run me');
+console.log("Run me");
 
 // Resize Event
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
   document.querySelector(
-    'h1'
+    "h1"
   ).innerText = `Resized to ${window.innerWidth} x ${window.innerHeight}`;
 });
 
 // Scroll Event
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   console.log(`Scrolled: ${window.scrollX} x ${window.scrollY}`);
 
   if (window.scrollY > 70) {
-    document.body.style.backgroundColor = 'black';
-    document.body.style.color = 'white';
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
   } else {
-    document.body.style.backgroundColor = 'white';
-    document.body.style.color = 'black';
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
   }
 });
 
 // Focus & Blur Events
-window.addEventListener('focus', () => {
-  document.querySelectorAll('p').forEach((p) => {
-    p.style.color = 'blue';
+window.addEventListener("focus", () => {
+  document.querySelectorAll("p").forEach((p) => {
+    p.style.color = "blue";
   });
 });
 
-window.addEventListener('blur', () => {
-  document.querySelectorAll('p').forEach((p) => {
-    p.style.color = 'black';
+window.addEventListener("blur", () => {
+  document.querySelectorAll("p").forEach((p) => {
+    p.style.color = "black";
   });
 });
