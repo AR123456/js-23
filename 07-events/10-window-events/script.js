cb444a631be3530a73a83e6742acbec8b62ef13b
AR123456 <coloradoz99@gmail.com>
2024-01-06 16:58:31 -0700

    scroll event

--- 07-events/10-window-events/script.js
+++ 07-events/10-window-events/script.js
@@ -11,3 +11,15 @@ window.addEventListener("resize", () => {
     "h1"
   ).innerText = `Resize to ${window.innerWidth}X${window.innerHeight}`;
 });
+// Scroll Event
+window.addEventListener("scroll", () => {
+  console.log(`Scrolled: ${window.scrollX} x ${window.scrollY}`);
+
+  if (window.scrollY > 70) {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+  } else {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+  }
+});
