f7e85118ef815a4bad1298b5f7b864223a5898dd
AR123456 <coloradoz99@gmail.com>
2024-01-07 09:17:04 -0700

    array method challenges

---
+++ Section 6 Loops Iteration and Array Metods/11-array-method-challenges/capWords.js
@@ -0,0 +1,22 @@
+/* 
+## Challenge 3
+**Instructions:**
+Create a new array called `capitalizedWords` with the words from the `words` array with the first letter of each word capitalized.
+**Expected Result:**
+```JavaScript
+const words = ['coder', 'programmer', 'developer'];
+console.log(capitalizedWords); // ['Coder', 'Programmer', 'Developer']
+``` */
+const words = ["coder", "programmer", "developer"];
+// const capitalizedWords = words.map((word) => {
+//   console.log(word.toLowerCase());
+//   let capFirst = word[0].toUpperCase();
+//   shortWord = word.slice(1);
+//   console.log(capFirst + shortWord);
+// });
+const capitalizedWords = words.map((word) => {
+  word.toLowerCase();
+  let capFirst = word[0].toUpperCase();
+  let shortWord = word.slice(1);
+  console.log(capFirst + shortWord);
+});
