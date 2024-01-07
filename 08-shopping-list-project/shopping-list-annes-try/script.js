bc9f5c189ef91c73d7ecb9119018e32863e12d00
AR123456 <coloradoz99@gmail.com>
2024-01-07 09:15:46 -0700

    shopping list psudo code

---
+++ 08-shopping-list-project/shopping-list-annes-try/script.js
@@ -0,0 +1,27 @@
+// declare relevant dom elemets
+const input = document.querySelector("#item-input");
+const addBtn = document.querySelector(".btn");
+const itemFilterInput = "";
+const liItem = "";
+const deleteButton = "";
+const clearAllBtn = "";
+
+// put event listener on the input
+
+// put event listioner on the button, on click
+// take the text from input, clean it up by stripping out spaces and things that are not letters or numbers.
+// make the first letter upper case
+//add the item as and li to the ul
+
+// deleting items -
+//add on click of x icon , when clicked allert do you want to del
+// remove item from the DOM ul
+
+// filter  add event listoner to the filter input
+// onkey -down or up or press call function that filters  by looking for that value in any of the list item text.  only show the match in the DOM
+// add any additional keys to the filter string
+
+// set up on click of the clear button that clears all the items from the DOM
+// edit mode ?
+// make it persist to local storage
+// deploy
