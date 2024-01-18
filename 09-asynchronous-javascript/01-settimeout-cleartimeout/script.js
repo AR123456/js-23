53e9db41e4d532f2cfe7192c1acaa4d81d226e0d
AR123456 <coloradoz99@gmail.com>
2024-01-18 06:40:04 -0700

    set time out in named function

--- 09-asynchronous-javascript/01-settimeout-cleartimeout/script.js
+++ 09-asynchronous-javascript/01-settimeout-cleartimeout/script.js
@@ -1,5 +1,10 @@
 //setTimeout()
 setTimeout(function () {
   console.log("hello from time out");
-}, 2000);
+}, 100);
 console.log("Hello I run first ");
+// can be a named function
+function changeText() {
+  document.querySelector("h1").innerHTML = "Hello from query selector callback";
+}
+setTimeout(changeText, 3000);
