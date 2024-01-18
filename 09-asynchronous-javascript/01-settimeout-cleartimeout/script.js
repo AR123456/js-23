64bf61f276472d1d15d851d939a3304d06230021
AR123456 <coloradoz99@gmail.com>
2024-01-18 07:03:45 -0700

    clearing time out

--- 09-asynchronous-javascript/01-settimeout-cleartimeout/script.js
+++ 09-asynchronous-javascript/01-settimeout-cleartimeout/script.js
@@ -8,3 +8,10 @@ function changeText() {
   document.querySelector("h1").innerHTML = "Hello from query selector callback";
 }
 setTimeout(changeText, 3000);
+// setTimeout returns an id can use it in set
+const timerID = setTimeout(changeText, 3000);
+// using clearTimeout added to event on  button
+document.querySelector("button").addEventListener("click", () => {
+  clearTimeout(timerID);
+  console.log("timer cancelled");
+});
