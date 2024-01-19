a02fc62f4e484550f3419bc24244a27efb821fed
AR123456 <coloradoz99@gmail.com>
2024-01-19 06:36:20 -0700

    can add a third param that is an arg to pass to the call back function

--- 09-asynchronous-javascript/02-setinterval-clearinterval/script.js
+++ 09-asynchronous-javascript/02-setinterval-clearinterval/script.js
@@ -1,9 +1,17 @@
-// set interval returns and id
-const intervalID = setInterval(theCallback, 1000);
+// set interval returns and id - here hello is an arg -
+// const intervalID = setInterval(theCallback, 1000, "Hello");
 
-function theCallback() {
-  console.log(Date.now());
+// function theCallback(hello) {
+//   console.log(hello, Date.now());
+// }
+
+// define storage of intervalID;
+let intervalID;
+
+function startChange() {
+  // check to see if the setInterval is running
 }
+
 function stopChange() {
   clearInterval(intervalID);
 }
