784ad1727919d92da46fa819086d4f76c5ee9d57
AR123456 <coloradoz99@gmail.com>
2024-01-19 06:29:35 -0700

    start and clear interval

--- 09-asynchronous-javascript/02-setinterval-clearinterval/script.js
+++ 09-asynchronous-javascript/02-setinterval-clearinterval/script.js
@@ -1,2 +1,10 @@
-const start = document.querySelector("#start");
-const stop = document.querySelector("#stop");
+// set interval returns and id
+const intervalID = setInterval(theCallback, 1000);
+
+function theCallback() {
+  console.log(Date.now());
+}
+function stopChange() {
+  clearInterval(intervalID);
+}
+document.getElementById("stop").addEventListener("click", stopChange);
