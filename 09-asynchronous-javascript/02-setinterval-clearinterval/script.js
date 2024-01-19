bf8bfe4f49d5fec0cccd5c1e0ae95779737bbb50
AR123456 <coloradoz99@gmail.com>
2024-01-19 06:24:37 -0700

    set interval

--- 09-asynchronous-javascript/02-setinterval-clearinterval/script.js
+++ 09-asynchronous-javascript/02-setinterval-clearinterval/script.js
@@ -1,10 +1,2 @@
 const start = document.querySelector("#start");
 const stop = document.querySelector("#stop");
-
-const startColorChange = () => {
-  //
-};
-const stopColorChange = () => {};
-
-start.addEventListener("click", setInterval(console.log("I started"), 2000));
-stop.addEventListener("click", stopColorChange);
