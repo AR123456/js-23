ddc227b679bbafd7fb6c785475d869243a882c75
AR123456 <coloradoz99@gmail.com>
2024-01-22 05:43:48 -0700

    passing function to callback

--- 09-asynchronous-javascript/03-callbacks/script.js
+++ 09-asynchronous-javascript/03-callbacks/script.js
@@ -9,11 +9,32 @@ const posts = [
   { title: "Post One", body: "This is post one" },
   { title: "Post Two", body: "This is post two" },
 ];
-
+// creating a post
+function createPost(post, cb) {
+  setTimeout(() => {
+    posts.push(post);
+    // make get posts a call back of createPost
+    cb();
+  }, 2000);
+}
 // grabbing set time out to mimic server request
 function getPosts() {
   setTimeout(() => {
     // this is a callback
-    posts.forEach(function () {});
+    posts.forEach(function (post) {
+      // create div
+      const div = document.createElement("div");
+      // add posts to div
+      div.innerHTML = `<strong>${post.title}</strong> - ${post.body}`;
+      // add div to the dom
+      document.querySelector("#posts").appendChild(div);
+    });
   }, 1000);
 }
+// this will get passed in as a callback here in create posts
+// getPosts();
+createPost(
+  { title: "Post Three", body: "This is post three" },
+  // passing in getPosts as callback
+  getPosts
+);
