d1e7d716c928245fd464d1e4181426ae37a75f39
AR123456 <coloradoz99@gmail.com>
2024-01-22 05:12:15 -0700

    call backs

--- 09-asynchronous-javascript/03-callbacks/script.js
+++ 09-asynchronous-javascript/03-callbacks/script.js
@@ -1,5 +1,19 @@
 const toggle = (e) => {
   e.target.classList.toggle("danger");
 };
-
+// a call back is a function that gets passed into another function
 document.querySelector("button").addEventListener("click", toggle);
+
+// fake server data for demo
+const posts = [
+  { title: "Post One", body: "This is post one" },
+  { title: "Post Two", body: "This is post two" },
+];
+
+// grabbing set time out to mimic server request
+function getPosts() {
+  setTimeout(() => {
+    // this is a callback
+    posts.forEach(function () {});
+  }, 1000);
+}
