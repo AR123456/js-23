f2b1921154f83b56679ef21e8c5bbb90d0c17f83
AR123456 <coloradoz99@gmail.com>
2024-01-31 07:05:04 -0700

    add items breaking into several functions

--- Section 9 Shopping List Project/shopping-list-start/script.js
+++ Section 9 Shopping List Project/shopping-list-start/script.js
@@ -20,11 +20,27 @@ function addItem(e) {
   // list item
   const li = document.createElement("li");
   li.appendChild(document.createTextNode(newItem));
-  // button
-  const button = document.createElement("button");
+  // button - a function that takes in css classes and makes a button
+  const button = createButton("remove-item btn-link text-red");
   li.appendChild(button);
   // add to dom
   itemList.appendChild(li);
+
+  // after adding to dom clear the input box
+  itemInput.value = "";
+}
+
+const createButton = (classes) => {
+  const button = document.createElement("button");
+  button.classNames = classes;
+  const icon = createIcon("fa-solid fa-xmark");
+  button.appendChild(icon);
+  return button;
+};
+function createIcon(classes) {
+  const icon = document.createElement("i");
+  icon.className = classes;
+  return icon;
 }
 // Event Listeners
 // listen for submit on form
