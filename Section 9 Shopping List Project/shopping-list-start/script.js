93a8d683a3214b8aa6af817414b1e7f5fc99b5e8
AR123456 <coloradoz99@gmail.com>
2024-01-31 07:05:31 -0700

    remove a single item event delegation

--- Section 9 Shopping List Project/shopping-list-start/script.js
+++ Section 9 Shopping List Project/shopping-list-start/script.js
@@ -25,14 +25,13 @@ function addItem(e) {
   li.appendChild(button);
   // add to dom
   itemList.appendChild(li);
-
   // after adding to dom clear the input box
   itemInput.value = "";
 }
 
 const createButton = (classes) => {
   const button = document.createElement("button");
-  button.classNames = classes;
+  button.className = classes;
   const icon = createIcon("fa-solid fa-xmark");
   button.appendChild(icon);
   return button;
@@ -42,6 +41,18 @@ function createIcon(classes) {
   icon.className = classes;
   return icon;
 }
+function removeItem(e) {
+  // click on x but delete the list item which is 2 parents up
+  // console.log(e.target.parentElement.parentElement);
+
+  // put this in an if to check if the remove-item class is on the button - not totally necessary for this use case
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    // traverse the DOM to get to the li
+    e.target.parentElement.parentElement.remove();
+  }
+}
 // Event Listeners
 // listen for submit on form
 itemForm.addEventListener("submit", addItem);
+// to del with the red x on the individual list item put the event on the item list ul and target what is inside that
+itemList.addEventListener("click", removeItem);
