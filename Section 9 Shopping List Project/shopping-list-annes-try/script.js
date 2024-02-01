ebc9d50fd177dfc30e1ff870ce7103a03298976c
AR123456 <coloradoz99@gmail.com>
2024-02-01 05:59:57 -0700

    help from chat gpt that is not helpful

--- Section 9 Shopping List Project/shopping-list-annes-try/script.js
+++ Section 9 Shopping List Project/shopping-list-annes-try/script.js
@@ -1,42 +1,36 @@
-// declare relevant dom elemets
+// declare relevant dom elements
 const item = document.querySelector("#item-input");
-
 const itemFilterInput = document.querySelector("#filter");
-
-const deleteButtons = document.querySelectorAll(".remove-item");
-
 const clearBtn = document.querySelector("#clear");
 const form = document.querySelector("#item-form");
 const ul = document.querySelector("ul");
 const li = document.querySelectorAll("li");
+
 const onSubmit = (e) => {
   e.preventDefault();
-  // TODO validate input and cap first letter
   const formData = new FormData(form);
-  const item = formData.get("item");
-  // TODO put into local storage
-  createListItem(item);
-
+  const newItem = formData.get("item");
+  createListItem(newItem);
   form.reset();
 };
+
 const createListItem = (item) => {
-  // add to DOM
   const li = document.createElement("li");
   li.innerHTML = `
-${item}
- <button class="remove-item btn-link text-red">
-   <i class="fa-solid fa-xmark"></i>
- </button>
-`;
+    ${item}
+    <button class="remove-item btn-link text-red">
+      <i class="fa-solid fa-xmark"></i>
+    </button>
+  `;
   document.querySelector(".items").appendChild(li);
 };
-// deleting items -
+
 const onDelete = () => {
   console.log("you hit the red x");
 };
 
 const onClear = () => {
-  if (confirm("You are about to delete all, are you sure?") == true) {
+  if (confirm("You are about to delete all, are you sure?")) {
     while (ul.firstChild) {
       ul.removeChild(ul.firstChild);
     }
@@ -45,22 +39,14 @@ const onClear = () => {
 
 // on submit
 form.addEventListener("submit", onSubmit);
+
 // put event listener on the clearBTN
 clearBtn.addEventListener("click", onClear);
-// delete individual items
-deleteButtons.forEach(function (button) {
-  // button.addEventListener("click", function () {
-  //   // why not removing dynamically added ?
-  //   const liItem = button.parentElement;
-  //   liItem.remove();
-  // });
-  ul.addEventListener("click", function (e) {
-    if (e.target.classList.contains("remove-item")) {
-      // get the parent element (li) of the clicked button
-      const liItem = e.target.parentElement;
 
-      // remove the dynamically added li element
-      liItem.remove();
-    }
-  });
+// delete individual items using event delegation
+ul.addEventListener("click", function (e) {
+  if (e.target.classList.contains("remove-item")) {
+    const liItem = e.target.parentElement;
+    liItem.remove();
+  }
 });
