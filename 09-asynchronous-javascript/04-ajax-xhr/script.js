466910076090a4f4bacb4ae28c5aa63ad36b40cb
AR123456 <coloradoz99@gmail.com>
2024-02-05 07:31:35 -0700

    logging the xhr readyState and status

--- 09-asynchronous-javascript/04-ajax-xhr/script.js
+++ 09-asynchronous-javascript/04-ajax-xhr/script.js
@@ -3,3 +3,10 @@ const xhr = new XMLHttpRequest();
 // need type of request and the url
 xhr.open("GET", "./movies.json");
 // event handler
+xhr.onreadystatechange = function () {
+  // 5 possible states 0-4  - state 4 is finished
+  console.log(this.readyState);
+  console.log(this.status);
+};
+
+xhr.send();
