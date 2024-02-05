5972c8c823f54d1438a9c98d75b2082ddc3250f8
AR123456 <coloradoz99@gmail.com>
2024-02-05 07:34:13 -0700

    check state and status before moving on

--- 09-asynchronous-javascript/04-ajax-xhr/script.js
+++ 09-asynchronous-javascript/04-ajax-xhr/script.js
@@ -4,9 +4,12 @@ const xhr = new XMLHttpRequest();
 xhr.open("GET", "./movies.json");
 // event handler
 xhr.onreadystatechange = function () {
-  // 5 possible states 0-4  - state 4 is finished
-  console.log(this.readyState);
-  console.log(this.status);
+  // 5 possible states 0-4  - state 4 is finished, status 200 all ok
+
+  // make sure that the request is finished and status is successful
+  if (this.readyState === 4 && this.status === 200) {
+    // ready to proceed
+  }
 };
 
 xhr.send();
