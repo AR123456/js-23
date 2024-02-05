becad1035dbbe0818cd8adc33d84f390376b1406
AR123456 <coloradoz99@gmail.com>
2024-02-05 07:57:42 -0700

    now getting brads data from git hub

--- 09-asynchronous-javascript/04-ajax-xhr/script.js
+++ 09-asynchronous-javascript/04-ajax-xhr/script.js
@@ -3,6 +3,7 @@ const ul = document.querySelector("#results");
 const xhr = new XMLHttpRequest();
 // need type of request and the url
 xhr.open("GET", "./movies.json");
+
 // event handler
 xhr.onreadystatechange = function () {
   // 5 possible states 0-4  - state 4 is finished, status 200 all ok
