665638518447482eaea230796085c4fbe2234384
AR123456 <coloradoz99@gmail.com>
2024-02-06 06:31:17 -0700

    joke on click button

--- 09-asynchronous-javascript/05-joke-generator-challenge/chuck-joke-generator-start/script.js
+++ 09-asynchronous-javascript/05-joke-generator-challenge/chuck-joke-generator-start/script.js
@@ -1,21 +1,28 @@
 // output to #results
 const joke = document.querySelector("#joke");
 const endpoint = "https://api.chucknorris.io/jokes/random";
+const jokeBtn = document.querySelector("#joke-btn");
 
-// initiate HTML request object
-const xhr = new XMLHttpRequest();
-xhr.open("GET", endpoint);
+// get joke
+const getJoke = () => {
+  // initiate HTML request object
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", endpoint);
 
-xhr.onreadystatechange = function () {
-  // wait for ready state 4
-  if (this.readyState === 4) {
-    // if status is 200 then write response to dom
-    if (this.status === 200) {
-      // need .value
-      const data = JSON.parse(this.responseText).value;
-      joke.innerHTML = `${data}`;
+  xhr.onreadystatechange = function () {
+    // wait for ready state 4
+    if (this.readyState === 4) {
+      // if status is 200 then write response to dom
+      if (this.status === 200) {
+        // need .value
+        const data = JSON.parse(this.responseText).value;
+        joke.innerHTML = `${data}`;
+      }
     }
-  }
-};
+  };
 
-xhr.send();
+  xhr.send();
+};
+jokeBtn.addEventListener("click", getJoke);
+// add a joke on page load
+document.addEventListener("DOMContentLoaded", getJoke);
