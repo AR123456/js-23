db5995df9fe019718762bafe1f2e3aab1a59e6a9
AR123456 <coloradoz99@gmail.com>
2024-02-06 06:25:10 -0700

    value writes to dom

--- 09-asynchronous-javascript/05-joke-generator-challenge/chuck-joke-generator-start/script.js
+++ 09-asynchronous-javascript/05-joke-generator-challenge/chuck-joke-generator-start/script.js
@@ -7,8 +7,15 @@ const xhr = new XMLHttpRequest();
 xhr.open("GET", endpoint);
 
 xhr.onreadystatechange = function () {
-  const data = JSON.parse(this.responseText);
-  joke.innerHTML = `${data}`;
+  // wait for ready state 4
+  if (this.readyState === 4) {
+    // if status is 200 then write response to dom
+    if (this.status === 200) {
+      // need .value
+      const data = JSON.parse(this.responseText).value;
+      joke.innerHTML = `${data}`;
+    }
+  }
 };
 
 xhr.send();
