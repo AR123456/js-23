77b4bedab7c48e19767d87315cb3018e2d3b8050
AR123456 <coloradoz99@gmail.com>
2024-02-06 06:10:05 -0700

    logging response from API

--- 09-asynchronous-javascript/05-joke-generator-challenge/chuck-joke-generator-start/script.js
+++ 09-asynchronous-javascript/05-joke-generator-challenge/chuck-joke-generator-start/script.js
@@ -1,10 +1,14 @@
 // output to #results
-const results = document.querySelector("#results");
+const joke = document.querySelector("#joke");
 const endpoint = "https://api.chucknorris.io/jokes/random";
 
 // initiate HTML request object
 const xhr = new XMLHttpRequest();
 xhr.open("GET", endpoint);
 
+xhr.onreadystatechange = function () {
+  const data = JSON.parse(this.responseText);
+  console.log(data.value);
+};
+
 xhr.send();
-console.log(this.responseText);
