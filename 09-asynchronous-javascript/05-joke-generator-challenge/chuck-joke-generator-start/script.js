a6e75af3d40cddcc27011baf8ee7fc3d9145c427
AR123456 <coloradoz99@gmail.com>
2024-02-06 06:04:07 -0700

    make endpoint const

--- 09-asynchronous-javascript/05-joke-generator-challenge/chuck-joke-generator-start/script.js
+++ 09-asynchronous-javascript/05-joke-generator-challenge/chuck-joke-generator-start/script.js
@@ -1,3 +1,10 @@
 // output to #results
 const results = document.querySelector("#results");
 const endpoint = "https://api.chucknorris.io/jokes/random";
+
+// initiate HTML request object
+const xhr = new XMLHttpRequest();
+xhr.open("GET", endpoint);
+
+xhr.send();
+console.log(this.responseText);
