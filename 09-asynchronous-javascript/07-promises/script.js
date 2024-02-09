83b4062374d63082a4c254cf4a19da8fc6f7d2f9
AR123456 <coloradoz99@gmail.com>
2024-02-09 06:33:38 -0700

    handling the error with .catch()

--- 09-asynchronous-javascript/07-promises/script.js
+++ 09-asynchronous-javascript/07-promises/script.js
@@ -11,7 +11,9 @@ const promise = new Promise((resolve, reject) => {
 // promise resovled so .then it takes in anything that is passed to resolve or could console.log something
 // the .then runs after the promise resolves
 promise.then(() => {
-  console.log("Promise in the .then aka promise consumed");
+  console.log(
+    "Promise in the .then of the first function aka promise consumed"
+  );
 });
 //Another syntax is to put the .then right at the end of the promise
 const getUser = new Promise((resolve, reject) => {
@@ -24,9 +26,12 @@ const getUser = new Promise((resolve, reject) => {
       // simulating something coming back from an API
       resolve({ name: "John", age: 30 });
     } else {
-      reject("Error goes here");
+      reject("Error in the reject()");
     }
   }, 1000);
 });
-getUser.then((user) => console.log(user));
+getUser
+  .then((user) => console.log(user))
+  // put the reject in a .catch that takes in the reject message from the promise
+  .catch((error) => console.log(error));
 console.log("Hello from global scope");
