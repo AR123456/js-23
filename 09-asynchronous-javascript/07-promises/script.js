21ec58431fb7d4be0372b4154a0082227a4b1057
AR123456 <coloradoz99@gmail.com>
2024-02-09 06:19:39 -0700

    another syntax

--- 09-asynchronous-javascript/07-promises/script.js
+++ 09-asynchronous-javascript/07-promises/script.js
@@ -9,6 +9,17 @@ const promise = new Promise((resolve, reject) => {
 });
 
 // promise resovled so .then it takes in anything that is passed to resolve or could console.log something
+// the .then runs after the promise resolves
 promise.then(() => {
   console.log("Promise in the .then aka promise consumed");
 });
+//Another syntax is to put the .then right at the end of the promise
+new Promise((resolve, reject) => {
+  // simulating an async task with set time out
+  // Do some async task
+  setTimeout(() => {
+    console.log("Async task 2complete");
+    resolve();
+  }, 1000);
+}).then(() => console.log("promise 2 consumed"));
+console.log("Hello from global scope");
