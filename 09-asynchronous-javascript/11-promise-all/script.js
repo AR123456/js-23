a31a84af2d97a2ee3f1cc2e06f9d8ee12d01ef73
AR123456 <coloradoz99@gmail.com>
2024-02-13 05:50:34 -0700

    promise all takes in an array of promises

--- 09-asynchronous-javascript/11-promise-all/script.js
+++ 09-asynchronous-javascript/11-promise-all/script.js
@@ -19,17 +19,23 @@ function getData(endpoint) {
     }, Math.floor(Math.random() * 3000) + 1000);
   });
 }
+// var for movies , actors and directors promises
+const moviesPromise = getData("./movies.json");
+const actorsPromise = getData("./actors.json");
+const directorsPromise = getData("./directors.json");
+// takes in an array of promises
+Promise.all();
 
-getData("./movies.json")
-  .then((movies) => {
-    console.log(movies);
-    return getData("./actors.json");
-  })
-  .then((actors) => {
-    console.log(actors);
-    return getData("./directors.json");
-  })
-  .then((directors) => {
-    console.log(directors);
-  })
-  .catch((error) => console.log(error));
+// getData("./movies.json")
+//   .then((movies) => {
+//     console.log(movies);
+//     return getData("./actors.json");
+//   })
+//   .then((actors) => {
+//     console.log(actors);
+//     return getData("./directors.json");
+//   })
+//   .then((directors) => {
+//     console.log(directors);
+//   })
+//   .catch((error) => console.log(error));
