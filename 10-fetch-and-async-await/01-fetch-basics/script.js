0495d875fa2e1cf60e7d429b193e2947bcf1596d
AR123456 <coloradoz99@gmail.com>
2024-02-14 05:26:37 -0700

    fetch to github

--- 10-fetch-and-async-await/01-fetch-basics/script.js
+++ 10-fetch-and-async-await/01-fetch-basics/script.js
@@ -10,6 +10,7 @@ fetch("./test.txt")
   .then((response) => response.text())
   .then((data) => console.log(data));
 // fetching from an API someone elses or your own backend
-fetch("http://api.github.com/users")
+fetch("http://api.github.com/users/mojombo")
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  //   .then((data) => console.log(data))
+  .then((data) => (document.querySelector("h1").textContent = data.login));
