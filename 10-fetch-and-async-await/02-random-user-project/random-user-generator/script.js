735ac0013c3af737035f1811cea876c37d24c5b3
AR123456 <coloradoz99@gmail.com>
2024-02-15 07:14:32 -0700

    mine in html

--- 10-fetch-and-async-await/02-random-user-project/random-user-generator/script.js
+++ 10-fetch-and-async-await/02-random-user-project/random-user-generator/script.js
@@ -1,5 +1,32 @@
 // fetch data
 fetch("https://randomuser.me/api/")
   .then((response) => response.json())
-  .then((data) => console.log(data.results));
-//   .then((data) => (document.querySelector("h1").textContent = data));
+
+  .then((data) => {
+    document.querySelector(".flex").innerHTML = `
+    <div class="flex">
+    <img
+      class="w-48 h-48 rounded-full mr-8"
+      src="https://randomuser.me/api/portraits/women/45.jpg"
+    />
+    <div class="space-y-3">
+      <p class="text-xl">
+        <span class="font-bold">Name: </span>${data.results[0].name.first} ${data.results[0].name.last} 
+      </p>
+      <p class="text-xl">
+        <span class="font-bold">Email: </span> sarawilson@gmail.com
+      </p>
+      <p class="text-xl">
+        <span class="font-bold">Phone: </span> (555) 555-5555
+      </p>
+      <p class="text-xl">
+        <span class="font-bold">Location: </span> Boston MA
+      </p>
+      <p class="text-xl"><span class="font-bold">Age: </span> 30</p>
+    </div>
+  </div>
+  
+  
+  
+  `;
+  });
