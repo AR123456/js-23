94a057f21c7d7921634b14662fe8645befe3b2fa
AR123456 <coloradoz99@gmail.com>
2024-02-15 07:15:16 -0700

    formatting response

--- 10-fetch-and-async-await/02-random-user-project/random-user-generator/script.js
+++ 10-fetch-and-async-await/02-random-user-project/random-user-generator/script.js
@@ -21,7 +21,7 @@ fetch("https://randomuser.me/api/")
         <span class="font-bold">Phone: </span> ${data.results[0].phone}
       </p>
       <p class="text-xl">
-        <span class="font-bold">Location: </span> ${data.results[0].address}
+        <span class="font-bold">Location: </span> ${data.results[0].location.city},${data.results[0].location.state}
       </p>
       <p class="text-xl"><span class="font-bold">Age: </span>${data.results[0].age}</p>
     </div>
