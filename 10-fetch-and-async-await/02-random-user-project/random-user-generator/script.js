e314000ec24a26ca8390628d5059bb07a55b4cf2
AR123456 <coloradoz99@gmail.com>
2024-02-17 05:11:32 -0700

    adding brads solution

--- 10-fetch-and-async-await/02-random-user-project/random-user-generator/script.js
+++ 10-fetch-and-async-await/02-random-user-project/random-user-generator/script.js
@@ -1,46 +1,10 @@
-document.body.style.backgroundColor = "purple";
-const generate = document.getElementById("generate");
-const spinner = document.querySelector(".spinner");
-const getUser = () => {
-  // fetch data
-  fetch("https://randomuser.me/api/")
-    .then((response) => response.json())
-    .then(spinner.toggleAttribute("hidden"))
-
+function fetchUser() {
+  fetch("https://randomuser.me/api")
+    .then((res) => res.json())
     .then((data) => {
-      spinner.toggleAttribute("hidden");
-      console.log(data.results[0]);
-      if (data.results[0].gender === "female") {
-        document.body.style.backgroundColor = "purple";
-      } else if (data.results[0].gender === "male") {
-        document.body.style.backgroundColor = "blue";
-      }
-      document.querySelector(".flex").innerHTML = `
-      <div class="flex">
-      <img
-        class="w-48 h-48 rounded-full mr-8"
-        src= ${data.results[0].picture.large}
-      />
-      <div class="space-y-3">
-        <p class="text-xl">
-          <span class="font-bold">Name: </span>${data.results[0].name.first} ${data.results[0].name.last} 
-        </p>
-        <p class="text-xl">
-          <span class="font-bold">Email: </span> ${data.results[0].email}
-        </p>
-        <p class="text-xl">
-          <span class="font-bold">Phone: </span> ${data.results[0].phone}
-        </p>
-        <p class="text-xl">
-          <span class="font-bold">Location: </span> ${data.results[0].location.city},${data.results[0].location.state}
-        </p>
-        <p class="text-xl"><span class="font-bold">Age: </span>${data.results[0].dob.age}</p>
-      </div>
-    </div>
-    
-    
-    
-    `;
+      // passing this into display uer function
+
+      displayUser(data.results[0]);
     });
-};
-generate.addEventListener("click", getUser);
+}
+fetchUser();
