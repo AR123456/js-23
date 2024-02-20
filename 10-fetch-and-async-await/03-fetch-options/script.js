5eba3a0a2ecc104c51ae7d8183be2c4c91c5f307
AR123456 <coloradoz99@gmail.com>
2024-02-20 08:08:34 -0500

    making a post

--- 10-fetch-and-async-await/03-fetch-options/script.js
+++ 10-fetch-and-async-await/03-fetch-options/script.js
@@ -1,11 +1,18 @@
 // JSON placeholder for API testing and prototyping  https://jsonplaceholder.typicode.com/
-function createPost() {
-  fetch("https://jsonplaceholder.typicode.com/").then((response) => {
+createPost = () => {
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    // object with options
+    method: "POST",
+    body: JSON.stringify({
+      title: post.title,
+      body: post.body,
+    }),
+  }).then((response) => {
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
     console.log(response);
   });
-}
+};
 
 createPost();
