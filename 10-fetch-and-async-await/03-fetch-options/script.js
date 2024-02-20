f7fccc76b455d6bef262489d91ce38be1c1c65f0
AR123456 <coloradoz99@gmail.com>
2024-02-20 08:16:15 -0500

    making the post

--- 10-fetch-and-async-await/03-fetch-options/script.js
+++ 10-fetch-and-async-await/03-fetch-options/script.js
@@ -1,18 +1,23 @@
 // JSON placeholder for API testing and prototyping  https://jsonplaceholder.typicode.com/
-createPost = () => {
+// destructure
+createPost = ({ title, body }) => {
   fetch("https://jsonplaceholder.typicode.com/posts", {
     // object with options
     method: "POST",
     body: JSON.stringify({
-      title: post.title,
-      body: post.body,
+      title: title,
+      body: body,
     }),
-  }).then((response) => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    console.log(response);
-  });
+    // content type of the data being sent
+    headers: {
+      // could send a token here too
+      "Content-Type": "application/json",
+      token: "abc123",
+    },
+  })
+    // response object
+    .then((res) => res.json())
+    .then((data) => console.log(data));
 };
 
-createPost();
+createPost({ title: "My Post", body: "This is the Post" });
