4e7694a7129ea606413d54de5398ebd1810587f4
AR123456 <coloradoz99@gmail.com>
2024-02-20 08:01:14 -0500

    making a fetch request

--- 10-fetch-and-async-await/03-fetch-options/script.js
+++ 10-fetch-and-async-await/03-fetch-options/script.js
@@ -1,1 +1,11 @@
 // JSON placeholder for API testing and prototyping  https://jsonplaceholder.typicode.com/
+function createPost() {
+  fetch("https://jsonplaceholder.typicode.com/").then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    console.log(response);
+  });
+}
+
+createPost();
