08111441363804b3add30c05a6d8ae6e3ec1ca1f
AR123456 <coloradoz99@gmail.com>
2024-02-22 08:00:03 -0500

    on click not working

--- 10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
+++ 10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
@@ -1,5 +1,7 @@
 const todos = document.getElementById("todo-list");
-getTodos = () => {
+const getToDoButton = document.getElementById("get-todo");
+const button = document.createElement("button");
+const getTodos = () => {
   fetch("https://jsonplaceholder.typicode.com/todos?_limit=5", {
     method: "GET",
   })
@@ -12,4 +14,29 @@ getTodos = () => {
       });
     });
 };
+const getOneTodo = (e) => {
+  e.preventDefault();
+  fetch("https://jsonplaceholder.typicode.com/todos?_limit=1", {
+    method: "GET",
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((todo) => {
+        const button = document.createElement("button");
+        button.innerText = todo.title;
+        todos.append(button);
+      });
+    });
+};
+const showDone = (e) => {
+  // e.preventDefault();
+  // button.classList.add("dark");
+  console.log("clicked");
+};
+const removeIt = (e) => {
+  // remove button
+};
 getTodos();
+getToDoButton.addEventListener("click", getOneTodo);
+button.addEventListener("click", showDone);
+button.addEventListener("dblclick", removeIt);
--- 10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/todos.md
+++ 10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/todos.md
@@ -3,5 +3,5 @@ fetch them ?\_limit=5
 place in the dom
 show in a dark background if it is completed (completed set to true)
 add one todo by making a post request limit 1 - note the others will go away at this point
-use a put request to mark a todo as node
+use a put request to mark a todo as done
 double click a todo to delete it
