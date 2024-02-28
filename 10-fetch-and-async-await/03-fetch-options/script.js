747b03bf672fc42c4d7939f643a1a3af07da0289
AR123456 <coloradoz99@gmail.com>
2024-02-28 06:55:21 -0700

    fetch post options

--- 10-fetch-and-async-await/03-fetch-options/script.js
+++ 10-fetch-and-async-await/03-fetch-options/script.js
@@ -1,10 +1,12 @@
+// destructure out the title and body from the post
 const createPost = ({ title, body }) => {
   // json placeholder to test
   fetch("https://jsonplaceholder.typicode.com/posts", {
     // address method, body , headers of the post here REST standards - crud
     method: "POST",
-    // content goes in body
+    // content goes in body needs to be json string
     body: JSON.stringify({
+      // specify the parts of the post-post.title and post.body
       title,
       body,
     }),
