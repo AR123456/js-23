03cff93980d6ea43031237ccad78f050ceeea75f
AR123456 <coloradoz99@gmail.com>
2024-03-03 09:18:08 -0700

    creating intit function

--- 10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
+++ 10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
@@ -1,18 +1,32 @@
 const apiUrl = "https://jsonplaceholder.typicode.com/todos";
 
+const addTodoToDom = (todo) => {
+  const div = document.createElement("div");
+  div.appendChild(document.createTextNode(todo.title));
+  // custom data set attribute
+  div.setAttribute("data-id", todo.id);
+  if (todo.completed) {
+    div.classList.add("done");
+  }
+  document.getElementById("todo-list").appendChild(div);
+};
 const getTodos = () => {
   fetch(apiUrl + "?_limit=5")
     .then((res) => res.json())
     .then((data) => {
-      data.forEach((todo) => {
-        const div = document.createElement("div");
-        div.appendChild(document.createTextNode(todo.title));
-        if (todo.completed) {
-          div.classList.add("done");
-        }
-        document.getElementById("todo-list").appendChild(div);
-      });
+      data.forEach((todo) => addTodoToDom(todo));
     });
 };
 
-getTodos();
+// add event listoner to dom
+const createTodo = () => {
+  //
+};
+// fire this off when the page loads
+const init = () => {
+  document.addEventListener("DOMContentLoaded", getTodos);
+  //   submit event listoner on form
+  document.querySelector("#todo-form").addEventListener("submit", createTodo);
+};
+
+init();
