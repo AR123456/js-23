ff41d312fdf71a5038e5257730ab937785ab00c1
AR123456 <coloradoz99@gmail.com>
2024-03-03 07:59:06 -0700

    re start with the code along

--- 10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
+++ 10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
@@ -1,33 +1,11 @@
-const todoForm = document.getElementById("todo-form");
-const submitButton = document.querySelector("button");
-const todoInput = document.getElementById("title");
-const todoList = document.getElementById("todo-list");
+const apiUrl = "https://jsonplaceholder.typicode.com/todos";
 
 const getTodos = () => {
-  fetch("https://jsonplaceholder.typicode.com/todos/?_limit=5")
+  fetch(apiUrl + "?_limit=5")
     .then((res) => res.json())
-    // .then((data) => console.log(data))
-    .then((data) => data.forEach((todo) => showTodos(todo)));
+    .then((data) => {
+      console.log(data);
+    });
 };
-const addTodos = (e) => {
-  // e.preventDefault();
-  const getFormData = {
-    title: todoInput.value,
-  };
-  console.log(getFormData);
-  fetch("https://jsonplaceholder.typicode.com/todos/", {
-    method: "POST",
-    body: JSON.stringify(getFormData),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((res) => res.json())
-    // .then((data) => console.log(data))
-    .then((data) => showTodos(data));
-};
-const showTodos = (todo) => {
-  todoList.innerHTML += ` 
-  <div >${todo.title}</div>`;
-};
-submitButton.addEventListener("submit", addTodos());
+
+getTodos();
