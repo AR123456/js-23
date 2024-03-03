af17ac4a7535187558c315f72170bc61da00d2f0
AR123456 <coloradoz99@gmail.com>
2024-03-03 07:59:33 -0700

    constructing the div

--- 10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
+++ 10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
@@ -4,7 +4,14 @@ const getTodos = () => {
   fetch(apiUrl + "?_limit=5")
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
+      data.forEach((todo) => {
+        const div = document.createElement("div");
+        div.appendChild(document.createTextNode(todo.title));
+        if (todo.completed) {
+          div.classList.add("done");
+        }
+        document.getElementById("todo-list").appendChild(div);
+      });
     });
 };
 
