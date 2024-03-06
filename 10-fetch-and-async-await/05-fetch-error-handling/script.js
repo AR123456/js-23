3b8b706db22f77896ce3601c262427db5d0f0f24
AR123456 <coloradoz99@gmail.com>
2024-03-06 06:12:46 -0700

    check iif response is ok

--- 10-fetch-and-async-await/05-fetch-error-handling/script.js
+++ 10-fetch-and-async-await/05-fetch-error-handling/script.js
@@ -3,23 +3,23 @@
 // you have to check the response for the status and if it is not a 200 throw some meaningful errors.
 // .catch()does work for network errors
 // Check for specific code
-fetch("http://httpstat.us/200")
-  .then((response) => {
-    if (response.status === 404) {
-      throw new Error("Not Found");
-    } else if (response.status === 500) {
-      throw new Error("Server Error");
-    } else if (response.status !== 200) {
-      throw new Error("Request Failed");
-    }
-    return response;
-  })
-  .then(() => {
-    console.log("success");
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+// fetch("http://httpstat.us/200")
+//   .then((response) => {
+//     if (response.status === 404) {
+//       throw new Error("Not Found");
+//     } else if (response.status === 500) {
+//       throw new Error("Server Error");
+//     } else if (response.status !== 200) {
+//       throw new Error("Request Failed");
+//     }
+//     return response;
+//   })
+//   .then(() => {
+//     console.log("success");
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
 
 // Catch ONLY runs on a network error
 // fetch("http://hello123.net")
@@ -32,3 +32,20 @@ fetch("http://httpstat.us/200")
 //   .catch((error) => {
 //     console.log(error);
 //   });
+
+// Test with response.ok -
+fetch("http://httpstat.us/404")
+  .then((response) => {
+    // if the response is not ok then throw the error
+    if (!response.ok) {
+      throw new Error("Request Failed");
+    }
+    return response;
+  })
+  .then(() => {
+    console.log("success");
+  })
+  // use the catch here to log the error
+  .catch((error) => {
+    console.log(error);
+  });
