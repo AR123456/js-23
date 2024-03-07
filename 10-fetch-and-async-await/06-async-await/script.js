691859c10aff4559da786b8ebafeb07696d24b98
AR123456 <coloradoz99@gmail.com>
2024-03-07 05:26:02 -0700

    aysnc await syntax

---
+++ 10-fetch-and-async-await/06-async-await/script.js
@@ -0,0 +1,16 @@
+// async await is alternative to .then  most devs use async await at this point
+const promise = new Promise((resolve, reject) => {
+  // consume promise
+  setTimeout(() => {
+    resolve({ name: "John", age: 20 });
+  }, 1000);
+});
+//using .then
+// promise.then((data) => console.log(data));
+
+// using async await
+async function getPromise() {
+  const response = await promise;
+  console.log(response);
+}
+getPromise();
