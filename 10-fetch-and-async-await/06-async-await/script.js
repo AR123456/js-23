10f0d017c2ac06e5681a7764321fff79d936554f
AR123456 <coloradoz99@gmail.com>
2024-03-07 05:34:59 -0700

    example of nesting async await from chat gpt

--- 10-fetch-and-async-await/06-async-await/script.js
+++ 10-fetch-and-async-await/06-async-await/script.js
@@ -1,21 +1,45 @@
-// async await is alternative to .then  most devs use async await at this point
-const promise = new Promise((resolve, reject) => {
-  // consume promise
-  setTimeout(() => {
-    resolve({ name: "John", age: 20 });
-  }, 1000);
-});
-//using .then
-// promise.then((data) => console.log(data));
+// // async await is alternative to .then  most devs use async await at this point
+// const promise = new Promise((resolve, reject) => {
+//   // consume promise
+//   setTimeout(() => {
+//     resolve({ name: "John", age: 20 });
+//   }, 1000);
+// });
+// //using .then
+// // promise.then((data) => console.log(data));
 
-// using async await
-// async function getPromise() {
+// // using async await
+// // async function getPromise() {
+// //   const response = await promise;
+// //   console.log(response);
+// // }
+// const getPromise = async () => {
 //   const response = await promise;
 //   console.log(response);
-// }
-const getPromise = async () => {
-  const response = await promise;
-  console.log(response);
-};
+// };
 
-getPromise();
+// getPromise();
+
+async function outerFunction() {
+  console.log("Start outerFunction");
+
+  async function innerFunction() {
+    console.log("Start innerFunction");
+    const result = await someAsyncOperation();
+    console.log("Result from innerFunction:", result);
+  }
+
+  await innerFunction();
+
+  console.log("End outerFunction");
+}
+
+async function someAsyncOperation() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Async operation completed");
+    }, 1000);
+  });
+}
+
+outerFunction();
