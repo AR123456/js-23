aece510ae6702300f56f2e6c95540b037a09b1b0
AR123456 <coloradoz99@gmail.com>
2024-03-07 05:42:51 -0700

    fetching using async await

--- 10-fetch-and-async-await/06-async-await/script.js
+++ 10-fetch-and-async-await/06-async-await/script.js
@@ -1,45 +1,52 @@
-// // async await is alternative to .then  most devs use async await at this point
-// const promise = new Promise((resolve, reject) => {
-//   // consume promise
-//   setTimeout(() => {
-//     resolve({ name: "John", age: 20 });
-//   }, 1000);
-// });
-// //using .then
-// // promise.then((data) => console.log(data));
+// async await is alternative to .then  most devs use async await at this point
+const promise = new Promise((resolve, reject) => {
+  // consume promise
+  setTimeout(() => {
+    resolve({ name: "John", age: 20 });
+  }, 1000);
+});
+//using .then
+// promise.then((data) => console.log(data));
 
-// // using async await
-// // async function getPromise() {
-// //   const response = await promise;
-// //   console.log(response);
-// // }
-// const getPromise = async () => {
+// using async await
+// async function getPromise() {
 //   const response = await promise;
 //   console.log(response);
-// };
-
+// }
+const getPromise = async () => {
+  const response = await promise;
+  console.log(response);
+};
 // getPromise();
+////////////
+const getUsers = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  console.log(data);
+};
+getUsers();
+/////////////
+/// example of nested from chat gpt
+// async function outerFunction() {
+//   console.log("Start outerFunction");
 
-async function outerFunction() {
-  console.log("Start outerFunction");
+//   async function innerFunction() {
+//     console.log("Start innerFunction");
+//     const result = await someAsyncOperation();
+//     console.log("Result from innerFunction:", result);
+//   }
 
-  async function innerFunction() {
-    console.log("Start innerFunction");
-    const result = await someAsyncOperation();
-    console.log("Result from innerFunction:", result);
-  }
+//   await innerFunction();
 
-  await innerFunction();
+//   console.log("End outerFunction");
+// }
 
-  console.log("End outerFunction");
-}
+// async function someAsyncOperation() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve("Async operation completed");
+//     }, 1000);
+//   });
+// }
 
-async function someAsyncOperation() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("Async operation completed");
-    }, 1000);
-  });
-}
-
-outerFunction();
+// outerFunction();
