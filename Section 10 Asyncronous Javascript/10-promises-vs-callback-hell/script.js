6595a1c1ce79b1e5d4069dff159b4d5f862ec1a2
AR123456 <coloradoz99@gmail.com>
2024-03-20 05:22:32 -0600

    asynchronous javascript

---
+++ Section 10 Asyncronous Javascript/10-promises-vs-callback-hell/script.js
@@ -0,0 +1,52 @@
+// function getData(endpoint, cb) {
+// don't return the call back
+function getData(endpoint) {
+  // put all of this into a promise
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("GET", endpoint);
+
+    xhr.onreadystatechange = function () {
+      if (this.readyState === 4) {
+        if (this.status === 200) {
+          //   cb(JSON.parse(this.responseText));
+          // this becomes resolve
+          resolve(JSON.parse(this.responseText));
+        } else {
+          reject("Something went wrong");
+        }
+      }
+      {
+      }
+    };
+
+    setTimeout(() => {
+      xhr.send();
+    }, Math.floor(Math.random() * 3000) + 1000);
+  });
+}
+
+// getData("./movies.json", (data) => {
+//   console.log(data);
+//   getData("./actors.json", (data) => {
+//     console.log(data);
+//     getData("./directors.json", (data) => {
+//       console.log(data);
+//     });
+//   });
+// });
+getData("./movies.json")
+  .then((movies) => {
+    console.log(movies);
+    return getData("./actors.json");
+  })
+  .then((actors) => {
+    console.log(actors);
+    return getData("./directors.json");
+  })
+  .then((directors) => {
+    console.log(directors);
+  })
+  // handle the error in the catch- returns what is in the reject() above
+  .catch((error) => console.log(error));
