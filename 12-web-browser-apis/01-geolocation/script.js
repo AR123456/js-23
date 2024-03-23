e82aee4437ac063e2d53b7b811e5bb6b2a14331f
AR123456 <coloradoz99@gmail.com>
2024-03-23 16:16:32 -0600

    geo location api

--- 12-web-browser-apis/01-geolocation/script.js
+++ 12-web-browser-apis/01-geolocation/script.js
@@ -1,4 +1,12 @@
-// getCurrentPosition()
-
-
-// watchPosition()
+const curSuccess = (pos) => {
+  const coords = pos.coords;
+  console.log(`Latitude: ${coords.latitude}`);
+  console.log(`Logitude: ${coords.longitude}`);
+  console.log(`Within: ${coords.accuracy} meters`);
+};
+const curError = (err) => {
+  // error object
+  console.log(`Error: ${err.code} = ${err.message}`);
+};
+const options = {};
+navigator.geolocation.getCurrentPosition(curSuccess, curError, options);
