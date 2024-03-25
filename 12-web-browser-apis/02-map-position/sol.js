0359bf5270ba3e5a9756960107fc971412f4aa7c
AR123456 <coloradoz99@gmail.com>
2024-03-25 06:43:01 -0600

    map position

--- 12-web-browser-apis/02-map-position/sol.js
+++ 12-web-browser-apis/02-map-position/sol.js
@@ -1,6 +1,6 @@
-const map = L.map("map").setView([0, 0], 2);
+const map = L.map('map').setView([0, 0], 2);
 
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
@@ -14,5 +14,5 @@ navigator.geolocation.getCurrentPosition(function (pos) {
   marker.setLatLng([lat, lng]).update();
   map.setView([lat, lng], 13);
 
-  marker.bindPopup("<strong>Hello World</strong> <br> This is my location");
+  marker.bindPopup('<strong>Hello World</strong> <br> This is my location');
 });
