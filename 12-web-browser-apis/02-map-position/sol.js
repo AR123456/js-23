6ea32b8926c35c4c69bb2bda7bfc22b4f0b55f6d
AR123456 <coloradoz99@gmail.com>
2024-03-25 06:43:43 -0600

    request animation frame

--- 12-web-browser-apis/02-map-position/sol.js
+++ 12-web-browser-apis/02-map-position/sol.js
@@ -1,33 +1,27 @@
-const canvas = document.getElementById('my-canvas');
-const ctx = canvas.getContext('2d');
-
-// Draw rectangle
-ctx.fillStyle = 'green';
-ctx.fillRect(10, 10, 150, 100);
-
-// Draw circle
-ctx.fillStyle = 'red';
-ctx.arc(300, 300, 100, 0, Math.PI * 2);
-ctx.fill();
-
-// Draw lines
-ctx.beginPath();
-ctx.strokeStyle = 'orange';
-ctx.lineWidth = 5;
-ctx.moveTo(10, 10);
-ctx.lineTo(300, 300);
-ctx.stroke();
-
-// Draw text
-ctx.font = '30px Arial';
-ctx.lineWidth = 1;
-ctx.fillStyle = 'blue';
-ctx.strokeStyle = 'blue';
-ctx.fillText('Hello World', 300, 100, 300);
-ctx.strokeText('Hello World', 300, 500, 300);
-
-// Draw image
 const image = document.querySelector('img');
-image.style.display = 'none';
+let start;
+let done = false;
 
-image.addEventListener('load', () => ctx.drawImage(image, 250, 250, 100, 100));
+function step(timestamp) {
+  if (start === undefined) {
+    start = timestamp;
+  }
+
+  const elapsed = timestamp - start;
+
+  if (elapsed > 5000) {
+    done = true;
+  }
+
+  if (done) {
+    return;
+  }
+
+  image.style.transform = `translateX(${elapsed / 10}px) rotate(${
+    elapsed / 10
+  }deg)`;
+
+  requestAnimationFrame(step);
+}
+
+requestAnimationFrame(step);
