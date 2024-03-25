2519e229b572c33b5bd2fc93df440f020f5d1746
AR123456 <coloradoz99@gmail.com>
2024-03-25 06:42:34 -0600

    geo location

--- 12-web-browser-apis/01-geolocation/script.js
+++ 12-web-browser-apis/01-geolocation/script.js
@@ -1,3 +1,4 @@
+// Get current position
 const curSuccess = (pos) => {
   const coords = pos.coords;
   console.log(`Latitude: ${coords.latitude}`);
@@ -8,5 +9,45 @@ const curError = (err) => {
   // error object
   console.log(`Error: ${err.code} = ${err.message}`);
 };
-const options = {};
-navigator.geolocation.getCurrentPosition(curSuccess, curError, options);
+const curOptions = {
+  enableHighAccuracy: true, //Use GPS if able
+  timeout: 5000, // Time to wait to stop trying for location
+
+  maximumAge: 0, // Do not use a cached position
+};
+navigator.geolocation.getCurrentPosition(curSuccess, curError, curOptions);
+
+// watchPosition()
+const target = {
+  latitude: 41.3874387,
+  longitude: -71.394839,
+};
+
+function watchSuccess(pos) {
+  const coords = pos.coords;
+  console.log(coords);
+
+  if (
+    target.latitude === coords.latitude &&
+    target.longitude === coords.longitude
+  ) {
+    console.log("You have reached your destination!");
+    navigator.geolocation.clearWatch(id);
+  }
+}
+
+const watchError = (err) => {
+  // error object
+  console.log(`Error: ${err.code} = ${err.message}`);
+};
+const watchOptions = {
+  enableHighAccuracy: true, //Use GPS if able
+  timeout: 5000, // Time to wait to stop trying for location
+
+  maximumAge: 0, // Do not use a cached position
+};
+navigator.geolocation.getCurrentPosition(
+  watchSuccess,
+  watchError,
+  watchOptions
+);
