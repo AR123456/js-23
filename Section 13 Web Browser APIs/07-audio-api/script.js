2ebfc3ead1b49fceef4bd62b99d7c82da66baa9f
AR123456 <coloradoz99@gmail.com>
2024-03-30 05:32:41 -0600

    controling the audio api

--- Section 13 Web Browser APIs/07-audio-api/script.js
+++ Section 13 Web Browser APIs/07-audio-api/script.js
@@ -2,4 +2,13 @@ const audio = document.getElementById("audio");
 const play = document.getElementById("play");
 const pause = document.getElementById("pause");
 const stop = document.getElementById("stop");
+const volume = document.getElementById("volume");
 const currentTime = document.getElementById("current-time");
+
+play.addEventListener("click", () => audio.play());
+pause.addEventListener("click", () => audio.pause());
+stop.addEventListener("click", () => {
+  audio.pause();
+  // resetting to 0 brings to begining of song
+  audio.currentTime = 0;
+});
