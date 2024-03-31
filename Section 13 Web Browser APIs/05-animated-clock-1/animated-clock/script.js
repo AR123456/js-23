906cc246c3bf39d2e101986343deafcd16d181ca
AR123456 <coloradoz99@gmail.com>
2024-03-31 07:03:23 -0600

    default styles

--- Section 13 Web Browser APIs/05-animated-clock-1/animated-clock/script.js
+++ Section 13 Web Browser APIs/05-animated-clock-1/animated-clock/script.js
@@ -7,7 +7,14 @@ function clock() {
   //canvas
 
   ctx.save(); // save the default state before the next repaint
-  ctx.clearRect(0, 0, 500, 500);
+  ctx.clearRect(0, 0, 500, 500); // wipe clean
+  ctx.translate(250, 250); // move zero point to the middle of the screen
+  ctx.rotate(-Math.PI / 2); //rotate clock face -90 deg
 
   ctx.restore(); // restore default state run last
+  //// default styles
+  ctx.strokeStyle = "#000000";
+  ctx.fillStyle = "#f4f4f4";
+  ctx.lineWidth = 5;
+  ctx.lineCap = "round";
 }
