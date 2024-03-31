fb09d3712bddca63dd747461cc4b045ed4a6738f
AR123456 <coloradoz99@gmail.com>
2024-03-31 07:03:43 -0600

    clock face

--- Section 13 Web Browser APIs/05-animated-clock-1/animated-clock/script.js
+++ Section 13 Web Browser APIs/05-animated-clock-1/animated-clock/script.js
@@ -11,10 +11,21 @@ function clock() {
   ctx.translate(250, 250); // move zero point to the middle of the screen
   ctx.rotate(-Math.PI / 2); //rotate clock face -90 deg
 
-  ctx.restore(); // restore default state run last
   //// default styles
   ctx.strokeStyle = "#000000";
   ctx.fillStyle = "#f4f4f4";
   ctx.lineWidth = 5;
   ctx.lineCap = "round";
+  // Draw clock face/border
+  ctx.save(); // save the state, everything before it
+  ctx.beginPath();
+  ctx.lineWidth = 14;
+  ctx.strokeStyle = "#800000";
+  ctx.arc(0, 0, 142, 0, Math.PI * 2, true); // circular  clock face
+  ctx.stroke();
+  ctx.fill();
+  ctx.restore();
+  //adding the tickmarks
+  ctx.restore(); // restore default state run last
 }
+clock();
