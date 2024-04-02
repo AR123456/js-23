fc2105d8831b2922556984889bd78aced71ab520
AR123456 <coloradoz99@gmail.com>
2024-04-02 07:16:01 -0600

    input values

--- Section 13 Web Browser APIs/05-animated-clock-1/animated-clock/script.js
+++ Section 13 Web Browser APIs/05-animated-clock-1/animated-clock/script.js
@@ -1,16 +1,19 @@
+const faceColor = document.getElementById("face-color").value;
+const borderColor = document.getElementById("border-color").value;
+const largeHandsColor = document.getElementById("largeHands-color").value;
+const lineColor = document.getElementById("line-color").value;
+const secondHandColor = document.getElementById("secondHand-color").value;
+
 // set up the clock in canvas- all this stuff runs after every repaint
 function clock() {
   const now = new Date();
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
-
   //canvas
-
   ctx.save(); // save the default state before the next repaint
   ctx.clearRect(0, 0, 500, 500); // wipe clean
   ctx.translate(250, 250); // move zero point to the middle of the screen
   ctx.rotate(-Math.PI / 2); //rotate clock face -90 deg
-
   //// default styles
   ctx.strokeStyle = "#000000";
   ctx.fillStyle = "#f4f4f4";
@@ -56,19 +59,47 @@ function clock() {
   const hr = now.getHours() % 12;
   const min = now.getMinutes();
   const sec = now.getSeconds();
-  console.log(`${hr}:${min}:${sec}`);
+  // console.log(`${hr}:${min}:${sec}`);
   ////  draw the hour hand
   ctx.save();
   ctx.rotate(
     (Math.PI / 6) * hr + (Math.PI / 360) * min + (Math.PI / 21600) * sec
   );
+  ctx.strokeStyle = "#800000";
+  ctx.lineWidth = 12;
   ctx.beginPath();
   ctx.moveTo(-20, 0);
   ctx.lineTo(80, 0);
   ctx.stroke();
   ctx.restore();
-
+  ////  draw the minute hand
+  ctx.save();
+  ctx.rotate((Math.PI / 30) * min + (Math.PI / 1800) * sec);
+  ctx.strokeStyle = "#800000";
+  ctx.lineWidth = 8;
+  ctx.beginPath();
+  ctx.moveTo(-28, 0);
+  ctx.lineTo(112, 0);
+  ctx.stroke();
+  ctx.restore();
+  //// draw second hand
+  ctx.save();
+  ctx.rotate((Math.PI / 30) * sec);
+  ctx.strokeStyle = "#FF7F50";
+  ctx.lineWidth = 4;
+  ctx.beginPath();
+  ctx.moveTo(-30, 0);
+  ctx.lineTo(100, 0);
+  ctx.stroke();
+  // arc in center
+  ctx.beginPath();
+  ctx.arc(0, 0, 10, 0, Math.PI * 2, true);
+  ctx.fillStyle = "#FF7F50";
+  ctx.fill();
+  ctx.restore();
   //
   ctx.restore(); // restore default state run last
+  // call requestAnimation here and outside of the clock function
+  requestAnimationFrame(clock);
 }
-clock();
+requestAnimationFrame(clock);
