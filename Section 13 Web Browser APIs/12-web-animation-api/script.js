2c412fb73c6d7ce539620de275a8ff550a70fd46
AR123456 <coloradoz99@gmail.com>
2024-04-04 05:52:48 -0600

    makig my sol more dry

--- Section 13 Web Browser APIs/12-web-animation-api/script.js
+++ Section 13 Web Browser APIs/12-web-animation-api/script.js
@@ -4,32 +4,41 @@ const pause = document.getElementById("pause");
 const reverse = document.getElementById("reverse");
 const speed = document.getElementById("speed-up");
 const slow = document.getElementById("slow-down");
+let rate = 1;
 
 // animations object
 const ballRoll = [
-  { transform: "rotate(0) translate3D(-50%, -50%, 0" },
-  { color: "white" },
-  { color: "blue" },
-  { transform: "rotate(360deg) translate3D(-50%, -50%, 0)" },
-  { color: "white" },
+  {
+    transform: "rotate(0) translate3D(-50%, -50%, 0)",
+    color: "white",
+  },
+  {
+    color: "blue",
+    offset: 0.3,
+  },
+  {
+    transform: "rotate(360deg) translate3D(-50%, -50%, 0)",
+    color: "white",
+  },
 ];
 const ballTiming = {
   duration: 3000,
   iterations: Infinity,
 };
-
+const roll = ball.animate(ballRoll, ballTiming);
+roll.pause();
 play.addEventListener("click", () => {
-  ball.animate(ballRoll, ballTiming);
+  roll.play();
 });
 pause.addEventListener("click", () => {
-  ball.animate(ballRoll, ballTiming).pause();
+  roll.pause();
 });
 reverse.addEventListener("click", () => {
-  ball.animate(ballRoll, ballTiming).reverse();
+  roll.reverse();
 });
 speed.addEventListener("click", () => {
-  ball.animate(ballRoll, ballTiming).pause();
+  roll.updatePlaybackRate(rate++);
 });
 slow.addEventListener("click", () => {
-  ball.animate(ballRoll, ballTiming).pause();
+  roll.updatePlaybackRate(rate--);
 });
