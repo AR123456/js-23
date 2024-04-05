fc8d711977d199f7845ac584939f89e6d7b0e5a2
AR123456 <coloradoz99@gmail.com>
2024-04-05 04:20:05 -0600

    video project

---
+++ Section 13 Web Browser APIs/11-video-player-project/video-player -v2 time stamp and control work/script.js
@@ -0,0 +1,23 @@
+const video = document.getElementById("video");
+const controls = document.getElementById("controls");
+const play = document.getElementById("play");
+const stop = document.getElementById("stop");
+const progress = document.getElementById("progress");
+const timestamp = document.getElementById("timestamp");
+
+const playVideo = () => {
+  video.play();
+};
+const stopVideo = () => {
+  video.pause();
+};
+
+const progressBar = () => {
+  console.log(progress.value);
+  console.log(video.duration);
+  console.log(video.duration / progress.value);
+};
+timestamp.innerHTML = video.timestamp;
+play.addEventListener("click", playVideo);
+stop.addEventListener("click", stopVideo);
+progress.addEventListener("change", progressBar);
