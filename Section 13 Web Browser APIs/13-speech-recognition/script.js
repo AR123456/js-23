df52bedc818ef0dbccc81eedf00408e7aea8a995
AR123456 <coloradoz99@gmail.com>
2024-04-08 06:58:30 -0400

    logging what I say

--- Section 13 Web Browser APIs/13-speech-recognition/script.js
+++ Section 13 Web Browser APIs/13-speech-recognition/script.js
@@ -6,11 +6,15 @@ const rec = new SpeechRecognition();
 
 rec.lang = "en-US";
 // if this is true api listions continuously
-rec.continuous = false;
+rec.continuous = true;
 
 // handler for what to do with result
 rec.onresult = function (e) {
   //   console.log(e.results[0][0].transcript);
+  for (let i = e.resultIndex; i < e.results.length; i++) {
+    const script = e.results[i][0].transcript;
+    console.log(script);
+  }
 };
 // browser will ask for permission
 rec.start();
