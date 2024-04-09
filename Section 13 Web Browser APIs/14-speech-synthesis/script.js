647f962bfabdc88f2c219d8f61dbcef31888fe3a
AR123456 <coloradoz99@gmail.com>
2024-04-09 07:22:32 -0400

    consts

---
+++ Section 13 Web Browser APIs/14-speech-synthesis/script.js
@@ -0,0 +1,5 @@
+const synth = window.speechSynthesis;
+inputForm = document.querySelector("#form");
+textInput = document.querySelector("#text-input");
+selectVoice = document.querySelector("#voice-select");
+speakButton = document.querySelector(".btn");
