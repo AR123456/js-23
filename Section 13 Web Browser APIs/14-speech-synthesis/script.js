7538ab245bd7b5a7e5703baa31df16ca9a785c38
AR123456 <coloradoz99@gmail.com>
2024-04-09 07:32:51 -0400

    my version working

--- Section 13 Web Browser APIs/14-speech-synthesis/script.js
+++ Section 13 Web Browser APIs/14-speech-synthesis/script.js
@@ -3,3 +3,11 @@ inputForm = document.querySelector("#form");
 textInput = document.querySelector("#text-input");
 selectVoice = document.querySelector("#voice-select");
 speakButton = document.querySelector(".btn");
+
+let voices = [];
+
+inputForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const sayThis = new SpeechSynthesisUtterance(textInput.value);
+  synth.speak(sayThis);
+});
