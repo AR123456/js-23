9d982129718e9b3edc2f160cf11a60bbd858de5c
AR123456 <coloradoz99@gmail.com>
2024-04-09 07:44:03 -0400

    speech synth

--- Section 13 Web Browser APIs/14-speech-synthesis/script.js
+++ Section 13 Web Browser APIs/14-speech-synthesis/script.js
@@ -4,7 +4,21 @@ textInput = document.querySelector("#text-input");
 selectVoice = document.querySelector("#voice-select");
 speakButton = document.querySelector(".btn");
 
-let voices = [];
+let voices;
+const addVoicesToSelect = () => {
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name} - ${voices[i].lang}`;
+
+    if (voices[i].default) {
+      option.textContent += " - DEFAULT";
+    }
+
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+    voiceSelect.appendChild(option);
+  }
+};
 const onSubmit = (e) => {
   e.preventDefault();
   const sayThis = new SpeechSynthesisUtterance(textInput.value);
