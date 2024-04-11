07f340019773e82c94ffc153e5c95020d36e95d4
AR123456 <coloradoz99@gmail.com>
2024-04-11 10:05:52 -0400

    what happens with new keyword

--- Section 14 Object Oriented Prototypes/02-constructor-functions/script.js
+++ Section 14 Object Oriented Prototypes/02-constructor-functions/script.js
@@ -1,3 +1,14 @@
 // consturctor function to create multiple instances of objects
 // new initializes a new constructor function
-const now = new Date();
+// const now = new Date();
+function Rectangle() {
+  //
+}
+
+// instanciate
+const rect1 = new Rectangle();
+// Using the new keyword 4 things happen
+// 1.	A new empty object is created
+// 2.	The constructor function is called with the arguments that we passed in.
+// 3.	The this keyword is set to the new empty object.
+// 4.	The new object is returned from the constructor function.
