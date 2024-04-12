34ef7b97ade6643f140e1b1fbc849e2207dfe627
AR123456 <coloradoz99@gmail.com>
2024-04-12 06:12:57 -0400

    boxing and un boxing

--- Section 14 Object Oriented Prototypes/03-literals-vs-constructors/script.js
+++ Section 14 Object Oriented Prototypes/03-literals-vs-constructors/script.js
@@ -5,3 +5,8 @@ console.log(strLit, typeof strLit);
 console.log(strObj, typeof strObj);
 // cannot use a method on a primitive data type
 //https://javascript.plainenglish.io/javascript-boxing-wrappers-5b5ff9e5f6ab
+//Boxing - js adds wrapper object onto literal
+console.log(strLit.toUpperCase());
+console.log(strLit[0]);
+//un boxing - object turned back into a literal
+console.log(strObj.valueOf(), typeof strObj.valueOf());
