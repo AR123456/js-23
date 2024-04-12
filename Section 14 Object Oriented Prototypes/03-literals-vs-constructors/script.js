3249a58545f73464eb13d12e022d309b8e19d8f9
AR123456 <coloradoz99@gmail.com>
2024-04-12 06:08:49 -0400

    boxing

---
+++ Section 14 Object Oriented Prototypes/03-literals-vs-constructors/script.js
@@ -0,0 +1,7 @@
+const strLit = "Hello";
+const strObj = new String("Hello from new String");
+
+console.log(strLit, typeof strLit);
+console.log(strObj, typeof strObj);
+// cannot use a method on a primitive data type
+//https://javascript.plainenglish.io/javascript-boxing-wrappers-5b5ff9e5f6ab
