9a802d9f5624d9bfc91faf33f743fcbb1775ceab
AR123456 <coloradoz99@gmail.com>
2024-04-12 05:37:14 -0400

    instantiate by passing into constructor

--- Section 14 Object Oriented Prototypes/02-constructor-functions/script.js
+++ Section 14 Object Oriented Prototypes/02-constructor-functions/script.js
@@ -1,15 +1,18 @@
-// consturctor function to create multiple instances of objects
+// constructor function to create multiple instances of objects
 // new initializes a new constructor function
 // const now = new Date();
-function Rectangle() {
-  //can set properties in here but more likely to set by passing in
-  this.name = "rectangle 1";
+// here can set up properties by passing them into the function
+function Rectangle(name, width, height) {
+  this.name = name;
+  this.width = width;
+  this.height = height;
 }
 
-// instanciate
-const rect1 = new Rectangle();
+// instantiate
+const rect1 = new Rectangle("rect2", 5, 7);
 // Using the new keyword 4 things happen
 // 1.	A new empty object is created
 // 2.	The constructor function is called with the arguments that we passed in.
 // 3.	The this keyword is set to the new empty object.
 // 4.	The new object is returned from the constructor function.
+console.log(rect1);
