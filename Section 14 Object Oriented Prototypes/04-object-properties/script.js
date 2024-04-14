c48fde2fcbbad372a5d0b10722879fbe8451cdf7
AR123456 <coloradoz99@gmail.com>
2024-04-14 08:31:37 -0400

    using backet or dot notation

---
+++ Section 14 Object Oriented Prototypes/04-object-properties/script.js
@@ -0,0 +1,19 @@
+// Instantiating a new Rectangle object with constructor function
+function Rectangle(name, width, height) {
+  this.name = name;
+  this.width = width;
+  this.height = height;
+  this.area = function () {
+    return this.width * this.height;
+  };
+}
+
+const rect1 = new Rectangle("Rectangle 1", 22, 20);
+const rect2 = new Rectangle("rectangle 2", 20, 30);
+
+console.log(rect1.name, rect2.width);
+// or bracket notation
+console.log(rect1["name"], rect2["height"]);
+// can add a new property
+rect1.color = "red";
+console.log(rect1.color);
