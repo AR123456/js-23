eae9c9dfdf0d0e0910102a46f5c5eab8214e4b53
AR123456 <coloradoz99@gmail.com>
2024-04-16 05:22:55 -0400

    getPrototypeOf

--- Section 14 Object Oriented Prototypes/05-prototypes/script.js
+++ Section 14 Object Oriented Prototypes/05-prototypes/script.js
@@ -8,3 +8,5 @@ function Rectangle(name, width, height) {
 }
 const rect = new Rectangle("Rect", 10, 10);
 console.log(rect);
+// getPrototypeOf - The Object.getPrototypeOf() static method returns the prototype (i.e. the value of the internal [[Prototype]] property) of the specified object.
+console.log(Object.getPrototypeOf(rect));
