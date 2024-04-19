260cb7efdec2f6a7ce74f50537ac8713e6f9829b
AR123456 <coloradoz99@gmail.com>
2024-04-19 08:13:39 -0400

    object of prototypes

---
+++ Section 14 Object Oriented Prototypes/07-object-create/script.js
@@ -0,0 +1,12 @@
+// object of prototypes
+const rectanglePrototypes = {
+  area: function () {
+    return this.width * this.height;
+  },
+  perimeter: function () {
+    return 2(this.width + this.height);
+  },
+  isSquare: function () {
+    return this.width === this.height;
+  },
+};
