dfff6991ac341b76d234eac16228e9533ecd09ee
AR123456 <coloradoz99@gmail.com>
2024-04-22 06:17:14 -0400

    object create

--- Section 14 Object Oriented Prototypes/07-object-create/script.js
+++ Section 14 Object Oriented Prototypes/07-object-create/script.js
@@ -10,3 +10,16 @@ const rectanglePrototypes = {
     return this.width === this.height;
   },
 };
+/// using the object create method in this function ()alternative to a constructor function)
+function createRectangle(height, width) {
+  return Object.create(rectanglePrototypes, {
+    height: {
+      value: height,
+    },
+    width: {
+      value: width,
+    },
+  });
+}
+const rect = createRectangle(10, 20);
+console.log(rect);
