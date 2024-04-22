c6fdd0cedd820e6f1537fbcdd9a6b1400104d81a
AR123456 <coloradoz99@gmail.com>
2024-04-22 06:54:56 -0400

    rect and circle

--- Section 14 Object Oriented Prototypes/08-prototypical-inheritance-call/script.js
+++ Section 14 Object Oriented Prototypes/08-prototypical-inheritance-call/script.js
@@ -1,5 +1,21 @@
 // shape object
 // likely that in JS one would use class for this in a large project
-function Shape() {
+function Shape(name) {
   this.name = name;
 }
+
+function Rectangle(name, height, width) {
+  Shape.call(this, name);
+
+  this.height = height;
+  this.width = width;
+}
+function Circle(name, radius) {
+  Shape.call(this, name);
+  this.radius = radius;
+}
+
+const rect = new Rectangle("Rectangle1", 20, 20);
+console.log(rect);
+const circle = new Circle("Circle 1", 30);
+console.log(circle);
