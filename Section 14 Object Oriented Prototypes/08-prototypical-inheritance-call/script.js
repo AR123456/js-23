f4cee4b2818632cca42473a6d1fa447e3af22b22
AR123456 <coloradoz99@gmail.com>
2024-04-22 06:59:28 -0400

    logging name with function

--- Section 14 Object Oriented Prototypes/08-prototypical-inheritance-call/script.js
+++ Section 14 Object Oriented Prototypes/08-prototypical-inheritance-call/script.js
@@ -1,8 +1,10 @@
-// shape object
-// likely that in JS one would use class for this in a large project
 function Shape(name) {
   this.name = name;
 }
+
+Shape.prototype.logName = function () {
+  console.log(`Shape Name: ${this.name}`);
+};
 
 function Rectangle(name, height, width) {
   Shape.call(this, name);
@@ -10,12 +12,31 @@ function Rectangle(name, height, width) {
   this.height = height;
   this.width = width;
 }
+
+// Inherits Shape prototypes
+Rectangle.prototype = Object.create(Shape.prototype);
+
+Rectangle.prototype.logName = function () {
+  console.log(`Rectangle Name: ${this.name}`);
+};
+
 function Circle(name, radius) {
   Shape.call(this, name);
+
   this.radius = radius;
 }
 
-const rect = new Rectangle("Rectangle1", 20, 20);
-console.log(rect);
-const circle = new Circle("Circle 1", 30);
-console.log(circle);
+// Inherits Shape prototypes
+Circle.prototype = Object.create(Shape.prototype);
+
+// Set prototype constructors
+Rectangle.prototype.constructor = Rectangle;
+Rectangle.prototype.constructor = Rectangle;
+
+const rect = new Rectangle("Rectangle 1", 20, 20);
+const cir = new Circle("Circle 1", 30);
+
+// console.log(rect, cir);
+
+rect.logName();
+cir.logName();
