ede0e6054ff4b0d505fed93dae726ead80d4b150
AR123456 <coloradoz99@gmail.com>
2024-04-23 07:58:12 -0400

    passing in defaults

--- Section 14 Object Oriented Prototypes/09-oop-challenge/script.js
+++ Section 14 Object Oriented Prototypes/09-oop-challenge/script.js
@@ -1,15 +1,16 @@
-function Player(name, lvl, points) {
+function Player(name) {
   this.name = name;
-  this.lvl = lvl;
-  this.points = points;
+  this.lvl = 1;
+  this.points = 0;
   this.gainXp = function (gain) {
     console.log(this.points, gain);
+    console.log(this.points + gain);
     return this.points + gain;
   };
   this.describe = function () {
     console.log(name);
     console.log(lvl);
-    console.log(points);
+    console.log(gainXp());
   };
 }
 let player1 = new Player("Bob", 0, 1);
