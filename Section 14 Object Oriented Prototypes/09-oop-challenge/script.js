b2c03e2bb29c4cce6cc61987b1a2bb2aa7595ad5
AR123456 <coloradoz99@gmail.com>
2024-04-23 07:57:12 -0400

    logging stuff

--- Section 14 Object Oriented Prototypes/09-oop-challenge/script.js
+++ Section 14 Object Oriented Prototypes/09-oop-challenge/script.js
@@ -6,8 +6,12 @@ function Player(name, lvl, points) {
     return points++;
   };
   this.describe = function () {
-    console.log("name");
-    console.log("lvl");
-    console.log("points");
+    console.log(name);
+    console.log(lvl);
+    console.log(points);
   };
 }
+let player1 = new Player("Bob");
+let player2 = new Player("Alice");
+player1.gainXp(5);
+player1.describe();
