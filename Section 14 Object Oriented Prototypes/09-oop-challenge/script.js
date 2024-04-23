e01b07b7187fc82f04ae01c9ff3f0979141a1ee1
AR123456 <coloradoz99@gmail.com>
2024-04-23 07:58:46 -0400

    sketching out the prototype functions

--- Section 14 Object Oriented Prototypes/09-oop-challenge/script.js
+++ Section 14 Object Oriented Prototypes/09-oop-challenge/script.js
@@ -2,17 +2,17 @@ function Player(name) {
   this.name = name;
   this.lvl = 1;
   this.points = 0;
-  this.gainXp = function (gain) {
-    console.log(this.points, gain);
-    console.log(this.points + gain);
-    return this.points + gain;
-  };
-  this.describe = function () {
-    console.log(name);
-    console.log(lvl);
-    console.log(gainXp());
-  };
 }
+Player.prototype.gainXp = function (gain) {
+  console.log(this.points, gain);
+  console.log(this.points + gain);
+  return this.points + gain;
+};
+Player.prototype.describe = function () {
+  console.log(name);
+  console.log(lvl);
+  console.log(gainXp());
+};
 let player1 = new Player("Bob", 0, 1);
 let player2 = new Player("Alice");
 player1.gainXp(5);
