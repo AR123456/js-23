6abbc78aa6e6c3d2ca9acb4a2d5aae133e97230e
AR123456 <coloradoz99@gmail.com>
2024-04-27 09:11:55 -0400

    call the static

--- 14-oop-classes-private-properties/03-static-methods/script.js
+++ 14-oop-classes-private-properties/03-static-methods/script.js
@@ -4,11 +4,18 @@ class Rectangle {
     this.height = height;
     this.width = width;
   }
+  // area cannot be static because we need height and width
   area() {
     return this.height * this.width;
   }
+  // this just returns the name of the rectangle
+  static getClass() {
+    return "This is my static method";
+  }
 }
-// instanciate the object to call it
+// instantiate the object to call it
 
 const rect = new Rectangle("rect", 10, 10);
 console.log(rect.area());
+// to call a static use the class itself, not the instance
+console.log(Rectangle.getClass());
