703c85a68ec85d839b70baec8ed6e99a2477c48b
AR123456 <coloradoz99@gmail.com>
2024-05-02 07:34:14 -0400

    private property convention

--- 14-oop-classes-private-properties/05-getters-setters-classes/script.js
+++ 14-oop-classes-private-properties/05-getters-setters-classes/script.js
@@ -1,9 +1,11 @@
 class Person {
   constructor(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
+    // the underscore by convention denotes a private property but is not required
+    this._firstName = firstName;
+    this._lastName = lastName;
   }
+  // use a getter to make upper case
 }
 
-const person1 = new Person("John", "Doe");
-console.log(person1);
+const person1 = new Person("john", "doe");
+console.log(person1._firstName);
