f682c15e0ea7c3b97fe08bd0d090b41058f91306
AR123456 <coloradoz99@gmail.com>
2024-05-01 20:52:00 -0400

    this keyword and bind

---
+++ 14-oop-classes-private-properties/04-bind-this/script.js
@@ -0,0 +1,11 @@
+class App {
+  constructor() {
+    this.serverName = "localhost";
+  }
+  getServerName() {
+    console.log(this.serverName);
+  }
+}
+
+const app = new App();
+app.getServerName();
