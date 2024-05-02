619aca0984e7ee8ee7fc78b5d737387aa2b2f4a9
AR123456 <coloradoz99@gmail.com>
2024-05-02 07:50:21 -0400

    using setter to make upper case

--- 14-oop-classes-private-properties/05-getters-setters-classes/script.js
+++ 14-oop-classes-private-properties/05-getters-setters-classes/script.js
@@ -5,13 +5,23 @@ class Person {
     this._firstName = firstName;
     this._lastName = lastName;
   }
-  // use a getter to make upper case
+  ///////////// use a getter to make upper case
   get firstName() {
     return this._firstName.charAt(0).toUpperCase() + this._firstName.slice(1);
   }
   // .firstName gets return and now first letter is Uppercase
+
+  ////////////// setter
+  // takes in the value we want to set here "this._firstName"
+  set firstName(value) {
+    // set to the value of this and make it upper case
+    this._firstName = value.charAt(0).toUpperCase() + value.slice(1);
+  }
 }
 
 const person1 = new Person("john", "doe");
-console.log(person1._firstName);
+// console.log(person1._firstName);
 console.log(person1.firstName);
+/// access the setter to make it uppercase
+person1.firstName = "joe";
+console.log(person1);
