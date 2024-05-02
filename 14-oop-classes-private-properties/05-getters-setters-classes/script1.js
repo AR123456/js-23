158886be2882abd122548ee4aedd0846bd34f075
AR123456 <coloradoz99@gmail.com>
2024-05-02 08:04:24 -0400

    making getter setter for last name

--- 14-oop-classes-private-properties/05-getters-setters-classes/script1.js
+++ 14-oop-classes-private-properties/05-getters-setters-classes/script1.js
@@ -1,4 +1,3 @@
-// DRY
 class Person {
   constructor(firstName, lastName) {
     // the underscore by convention denotes a private property but is not required
@@ -6,36 +5,23 @@ class Person {
     this._firstName = firstName;
     this._lastName = lastName;
   }
-  ///////////// getter
+  ///////////// use a getter to make upper case
   get firstName() {
-    return this.capitalizeFirst(this._firstName);
+    return this._firstName.charAt(0).toUpperCase() + this._firstName.slice(1);
   }
+  // .firstName gets return and now first letter is Uppercase
 
   ////////////// setter
   // takes in the value we want to set here "this._firstName"
   set firstName(value) {
-    this._firstName = this.capitalizeFirst(value);
-  }
-  ///////////// getter
-  get lastName() {
-    return this.capitalizeFirst(this._lastName);
-  }
-
-  ////////////// setter
-  // takes in the value we want to set here "this._firstName"
-  set lastName(value) {
-    this._lastName = this.capitalizeFirst(value);
-  }
-  // method to cap the first letter
-  capitalizeFirst(value) {
-    return value.charAt(0).toUpperCase() + value.slice(1);
+    // set to the value of this and make it upper case
+    this._firstName = value.charAt(0).toUpperCase() + value.slice(1);
   }
 }
 
 const person1 = new Person("john", "doe");
 // console.log(person1._firstName);
 console.log(person1.firstName);
-console.log(person1.lastName);
 /// access the setter to make it uppercase
 person1.firstName = "joe";
 console.log(person1);
