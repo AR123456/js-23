d0261726118d1715ff5c0c15c66a4605f3fef2cd
AR123456 <coloradoz99@gmail.com>
2024-05-08 07:39:23 -0400

    notes on what classes are responsible for

--- 15-tracalorie-project/trac-cals/js/app.js
+++ 15-tracalorie-project/trac-cals/js/app.js
@@ -1,10 +1,12 @@
 class CalorieTracker {
   //public addMeal,removeMeal,addWorkout,removeWorkout,resetDay,setLimit,loadItems
   // private displayCaloriesTotal,displayCalorieLimit, displayCaloriesConsumed,displayCaloriesBurned,displayCaloriesRemaining,displayNewMeal,displayNewWorkout,renderStats
+  // define const getElementById  displaying in dom in private Methods
 }
 class Meal {
   // constructor id,name,cal
   constructor(id, name, calories) {
+    //  this.id = Math.random().toString(16).slice(2);
     this.id = id;
     this.name = name;
     this.calories = calories;
@@ -23,6 +25,8 @@ class Storage {}
 class App {
   // constructor = Event listeners and initialize tracker
   // private  newItem, removeItem, filterItems, reset() and setLimit
+  // _loadEventListeners(){} - getElementById from forms,  addEventListener submit.  also the filters getElements and event listener of keyup ,  also reset and ue click as its event
+  // input validation in here on the _newItem method
 }
 
 const app = new App();
