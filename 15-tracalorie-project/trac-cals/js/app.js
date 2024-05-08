f20ae853eb34646704510f82dfa2eb505f6b9f91
AR123456 <coloradoz99@gmail.com>
2024-05-08 07:38:59 -0400

    meal and workout class

--- 15-tracalorie-project/trac-cals/js/app.js
+++ 15-tracalorie-project/trac-cals/js/app.js
@@ -4,9 +4,19 @@ class CalorieTracker {
 }
 class Meal {
   // constructor id,name,cal
+  constructor(id, name, calories) {
+    this.id = id;
+    this.name = name;
+    this.calories = calories;
+  }
 }
 class Workout {
   // constructor id,name,cal
+  constructor(id, name, calories) {
+    this.id = id;
+    this.name = name;
+    this.calories = calories;
+  }
 }
 class Storage {}
 // static methods - getCalorieLimit,setCalorieLimit,getTotalCalories, setTotalCalories,updateCalories,saveMeal,removeMeal,saveWorkout,removeWorkout,getMeals,getWorkouts,clearAll
