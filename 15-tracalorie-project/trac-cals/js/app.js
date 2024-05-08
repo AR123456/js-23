4a1175a89956758361c84b6decc4f8684f8ca1ed
AR123456 <coloradoz99@gmail.com>
2024-05-08 07:38:36 -0400

    some psudocode

---
+++ 15-tracalorie-project/trac-cals/js/app.js
@@ -0,0 +1,18 @@
+class CalorieTracker {
+  //public addMeal,removeMeal,addWorkout,removeWorkout,resetDay,setLimit,loadItems
+  // private displayCaloriesTotal,displayCalorieLimit, displayCaloriesConsumed,displayCaloriesBurned,displayCaloriesRemaining,displayNewMeal,displayNewWorkout,renderStats
+}
+class Meal {
+  // constructor id,name,cal
+}
+class Workout {
+  // constructor id,name,cal
+}
+class Storage {}
+// static methods - getCalorieLimit,setCalorieLimit,getTotalCalories, setTotalCalories,updateCalories,saveMeal,removeMeal,saveWorkout,removeWorkout,getMeals,getWorkouts,clearAll
+class App {
+  // constructor = Event listeners and initialize tracker
+  // private  newItem, removeItem, filterItems, reset() and setLimit
+}
+
+const app = new App();
