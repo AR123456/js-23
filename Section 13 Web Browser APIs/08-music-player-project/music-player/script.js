992f6ab3dc64ba5a88957079698d3e6660edbff6
AR123456 <coloradoz99@gmail.com>
2024-06-14 08:11:47 -0400

    not working

--- Section 13 Web Browser APIs/08-music-player-project/music-player/script.js
+++ Section 13 Web Browser APIs/08-music-player-project/music-player/script.js
@@ -1,16 +1,24 @@
-// psudo code
-// declare vars will need play pause next prev
-
-// looking a the css style when .play css is applied ot the music containter div image spins and progress bar appears
+// consts
+const audio = document.getElementById("audio");
 const play = document.getElementById("play");
 const prev = document.getElementById("prev");
 const next = document.getElementById("next");
+const progress = document.getElementById("progress");
 const musicContainer = document.getElementById("music-container");
+const pause = `<button id="play" class="action-btn action-btn-big">
+<i class="fas fa-pause"></i>
+</button>`;
 
 const playSong = () => {
   musicContainer.classList.add("play");
-  // change the play button to a pause button
-  play.innerHTML = `<i class="fas fa-pause"></i>`;
+  // change the play button to a pause button -
+  play.innerHTML = pause;
+  //play song
+  audio.play();
 };
 
 play.addEventListener("click", playSong);
+pause.addEventListener("click", () => audio.pause());
+audio.addEventListener("timeupdate", () => {
+  progress.innerText = audio.progress;
+});
