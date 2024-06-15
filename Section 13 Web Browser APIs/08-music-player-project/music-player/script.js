204c36733b3738e31ef4825ac2444dc2f4eabf54
AR123456 <coloradoz99@gmail.com>
2024-06-15 07:47:35 -0400

    wire on click to play button

--- Section 13 Web Browser APIs/08-music-player-project/music-player/script.js
+++ Section 13 Web Browser APIs/08-music-player-project/music-player/script.js
@@ -19,25 +19,6 @@ const loadSong = (song) => {
   audio.src = `music/${song}.mp3`;
   cover.src = `images/${song}.jpg`;
 };
-const playSong = () => {
-  musicContainer.classList.add("play");
-  playBtn.querySelector("i.fas").classList.remove("fa-play");
-  playBtn.querySelector("i.fas").classList.add("fa-pause");
-  audio.play();
-};
-const pauseSong = () => {
-  musicContainer.classList.remove("play");
-  playBtn.querySelector("i.fas").classList.add("fa-play");
-  playBtn.querySelector("i.fas").classList.remove("fa-pause");
-  audio.pause();
-};
-const prevSong = () => {
-  songIndex--;
-};
-const nextSong = () => {
-  songIndex++;
-};
-
 loadSong(songs[songIndex]);
 
 playBtn.addEventListener("click", () => {
@@ -49,6 +30,3 @@ playBtn.addEventListener("click", () => {
     playSong();
   }
 });
-
-prevBtn.addEventListener("click", prevSong);
-nextBtn.addEventListener("click", nextSong);
