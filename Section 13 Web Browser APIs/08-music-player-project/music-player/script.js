0126ac5d8d5db6debe98fc4d8e47f3c3e84d0780
AR123456 <coloradoz99@gmail.com>
2024-06-16 07:10:23 -0400

    next song

--- Section 13 Web Browser APIs/08-music-player-project/music-player/script.js
+++ Section 13 Web Browser APIs/08-music-player-project/music-player/script.js
@@ -34,9 +34,6 @@ const pauseSong = () => {
 };
 const prevSong = () => {
   songIndex--;
-  if (songIndex < 0) {
-    songIndex = songs.length - 1;
-  }
   loadSong(songs[songIndex]);
   playSong();
 };
