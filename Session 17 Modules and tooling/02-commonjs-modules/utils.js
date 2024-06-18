479500756c0af5ebe37f223b000140f48378efdb
AR123456 <coloradoz99@gmail.com>
2024-06-18 08:15:02 -0400

    another way to split a string

--- Session 17 Modules and tooling/02-commonjs-modules/utils.js
+++ Session 17 Modules and tooling/02-commonjs-modules/utils.js
@@ -1,14 +1,26 @@
 // cap the first word in the sentence
+// function capitalizeWords(str) {
+//   return (
+//     str
+//       // make everything lower case
+//       .toLowerCase()
+//       // turn into array
+//       .split(" ")
+//       // for each words first letter make upper case and then add the rest of the word
+//       .map((word) => word[0].toUpperCase() + word.substr(1))
+//       // turn the array backinto a string
+//       .join(" ")
+//   );
+// }
+// another way to make title case
 function capitalizeWords(str) {
   return (
     str
-      // make everything lower case
-      .toLowerCase()
-      // turn into array
+      //
       .split(" ")
-      // for each words first letter make upper case and then add the rest of the word
-      .map((word) => word[0].toUpperCase() + word.substr(1))
-      // turn the array backinto a string
+      .map((word) => {
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+      })
       .join(" ")
   );
 }
