dad6f0504e41d8305e0989efa7d56d18c3200239
AR123456 <coloradoz99@gmail.com>
2026-01-12 06:20:54 -0500

    capitalize challenge

---
+++ Challenges Worth Redoing/session 2-capitalize-challenge/capChal.js
@@ -0,0 +1,9 @@
+/* # Capitalize Challenge
+**Instructions**:
+Take the variable `myString` and capitalize the first letter of the word using some of the methods that we talked about in the last video. Put the result in a variable called `myNewString`.
+Create multiple solutions if you would like.
+**Expected Result:**
+```JavaScript
+const myString = 'developer';
+console.log(myNewString); // 'Developer'
+``` */
